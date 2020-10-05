b8b8fae4b0b769d9a45966c65c9e8ea79135cd8f
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-10-05 20:09:59 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -1,22 +1,33 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-print ("")
-print ("")
-print("""processing.......""")
+print("")
+print("""Welcome To HellBot String Generator By HellBot Official""")
+print("""Kindly Enter Your Details To Continue ! """)
 
-API_KEY = '1754367'
-API_HASH = "231b8cc6cca12ee51a85cf543321f476"
+API_KEY = input("API_KEY: ")
+API_HASH = input("API_HASH: ")
+
 while True:
-  try:
-   with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-      print("")
-      session = client.session.save()
-      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(tap to copy)ðŸ‘‡ \n\n `{session}`")
-      print("You telegram String session successfully stored in your telegram, please check your Telegram Saved Messages ")
-      print("Store it safe !! Don't share with anyone.. Regards.. Team DARK COBRA")
-  except:
-   print ("")
-   print ("Wrong phone number \n make sure its with correct  country code")
-   print ("")
-   continue
-  break
+ try:
+  with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
+   print(
+       "String Sent To Your Saved Message, Store It To A Safe Place!! "
+   )
+   print("")
+   session = client.session.save()
+   client.send_message(
+       "me",
+       f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n {session} \n\n And Visit @HellBot_Official For Any Help !"
+   )
+
+   print(
+       "Thanks for Choosing HellBot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+   )
+ except:
+  print("")
+  print(
+      "Wrong phone number \n make sure its with correct country code. Example : +919961998999 ! Kindly Retry"
+  )
+  print("")
+  continue
+ break
