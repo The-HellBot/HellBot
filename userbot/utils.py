7b96dfc10f4bbb447c24bb811a34d73b70f095c6
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-10-05 20:24:04 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -289,7 +289,7 @@ def errors_handler(func):
 
             text = "**USERBOT CRASH REPORT**\n\n"
 
-            link = "[here](https://t.me/sn12384)"
+            link = "[here](https://t.me/testpy12)"
             text += "If you wanna you can report it"
             text += f"- just forward this message {link}.\n"
             text += "Nothing is logged except the fact of error and date\n"
