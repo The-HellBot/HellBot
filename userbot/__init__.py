bac36b08103d8d24e657332efa49e5cf746b4f38
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-10-05 15:52:52 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -130,7 +130,7 @@ if bool(ENV):
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/DARK-COBRA/DARKCOBRA.git")
+    "https://github.com/HellBoy-OP/HellBot.git")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
