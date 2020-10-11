0d9f5c81d91b65462cceab1f0e5e400af9271547
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-10-11 11:46:12 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -152,7 +152,7 @@ async def carbon_api(e):
 
          file,
 
-         caption="<< Here's your carbon, \n Carbonised by [DARKCOBRA](https://www.github.com/hellboi-atul/hellboi-atul)>> ",
+         caption="<< Here's your carbon, \n Carbonised by [Hêllẞø†](https://t.me/hellbot_official)>> ",
 
          force_document=True,
 
