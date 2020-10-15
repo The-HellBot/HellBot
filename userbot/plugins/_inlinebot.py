658ef9385f3486da993952b5becaad8f63a0e930
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-10-16 02:50:57 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -1,123 +1,140 @@
-from math import ceil
-import asyncio
-import json
-import random
 import re
-from telethon import events, errors, custom
+from math import ceil
+from userbot.plugins import inlinestats
+from telethon import custom, events, Button
+from userbot import ALIVE_NAME
 from userbot import CMD_LIST
-import io
 
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
+
     @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
     async def inline_handler(event):
         builder = event.builder
         result = None
         query = event.text
-        if event.query.user_id == bot.uid and query.startswith("Userbot"):
+        if event.query.user_id == bot.uid and query.startswith("Hell User"):
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                "@HellBot_Official",
-                text="{}\nCurrently Loaded Plugins: {}".format(
-                    query, len(CMD_LIST)),
+                "© Userbot Help",
+                text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),
                 buttons=buttons,
-                link_preview=False
+                link_preview=False,
             )
+        if event.query.user_id == bot.uid and query == "stats":
+           result = builder.article(
+           title="Stats",
+           text=f"**Showing Stats For {DEFAULTUSER}'s HellBot** \nNote --> Only Owner Can Check This \n(C) @HellBot_Official",
+           buttons = [
+                   [custom.Button.inline("Show Stats 🚶", data="terminator")],
+                   [Button.url("Repo 🛡️", "https://github.com/HellBoy-OP/HellBot")],
+                   [Button.url("Join Channel 📃", "t.me/hellbot_official")],
+             ]
+         )
         await event.answer([result] if result else None)
-    @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-        data=re.compile(b"helpme_next\((.+?)\)")
-    ))
+    @tgbot.on(
+        events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+            data=re.compile(b"helpme_next\((.+?)\)")
+        )
+    )
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:  # pylint:disable=E0602
-            current_page_number = int(
-                event.data_match.group(1).decode("UTF-8"))
-            buttons = paginate_help(
-                current_page_number + 1, CMD_LIST, "helpme")
+            current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+            buttons = paginate_help(current_page_number + 1, CMD_LIST, "helpme")
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use Mine\n Check @HellBot_Official For The Source!"
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+            reply_popp_up_alert = "Apna Hellbot bana jake, mera use naa kar!😏"
+            await event.answer(reply_popp_up_alert, cache_time=0, alert=True)
 
-
-    @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-        data=re.compile(b"helpme_prev\((.+?)\)")
-    ))
+    @tgbot.on(
+        events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+            data=re.compile(b"helpme_prev\((.+?)\)")
+        )
+    )
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid:  # pylint:disable=E0602
-            current_page_number = int(
-                event.data_match.group(1).decode("UTF-8"))
+            current_page_number = int(event.data_match.group(1).decode("UTF-8"))
             buttons = paginate_help(
-                current_page_number - 1,
-                CMD_LIST,  # pylint:disable=E0602
-                "helpme"
+                current_page_number - 1, CMD_LIST, "helpme"  # pylint:disable=E0602
             )
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use Mine\n Check @HellBot_Official For The Source!"
+            reply_pop_up_alert = "Apna HellBot Bana jake, mera use naa kar!😏"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-    @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-        data=re.compile(b"us_plugin_(.*)")
-    ))
+
+    @tgbot.on(
+        events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+            data=re.compile(b"us_plugin_(.*)")
+        )
+    )
     async def on_plug_in_callback_query_handler(event):
-        plugin_name = event.data_match.group(1).decode("UTF-8")
-        help_string = ""
-        try:
-            for i in CMD_LIST[plugin_name]:
-                help_string += i
-                help_string += "\n"
-        except:
-            pass
-        if help_string is "":
-            reply_pop_up_alert = "{} is useless".format(plugin_name)
+        if event.query.user_id == bot.uid:
+            plugin_name = event.data_match.group(1).decode("UTF-8")
+            help_string = ""
+            try:
+                for i in CMD_LIST[plugin_name]:
+                    help_string += i
+                    help_string += "\n"
+            except:
+                pass
+            if help_string is "":
+                reply_pop_up_alert = "{} is useless".format(plugin_name)
+            else:
+                reply_pop_up_alert = help_string
+            reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
+                © Hellbot".format(
+                plugin_name
+            )
+            try:
+                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+            except:
+                halps = "Do .help {} to get the list of commands.".format(plugin_name)
+                await event.answer(halps, cache_time=0, alert=True)
         else:
-            reply_pop_up_alert = help_string
-        reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
-            © @HellBot_Official".format(plugin_name)
-        try:
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-        except:
-            with io.BytesIO(str.encode(reply_pop_up_alert)) as out_file:
-                out_file.name = "{}.txt".format(plugin_name)
-                await event.client.send_file(
-                    event.chat_id,
-                    out_file,
-                    force_document=True,
-                    allow_cache=False,
-                    caption="Uploaded By @HellBot_Official"
-                )
+            reply_pop_up_alert = "Apna HellBot bana jake, mera use naa kar!😏"
 
-
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"terminator")))
+    async def rip(event):
+            if event.query.user_id == bot.uid:
+                text = inlinestats
+                await event.answer(text, alert=True)
+            else:
+                txt = "You Can't View My Masters Stats"
+                await event.answer(txt, alert=True)
+                
 def paginate_help(page_number, loaded_plugins, prefix):
-    number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
-    number_of_cols = Config.NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD
-    multi = Config.EMOJI_TO_DISPLAY_IN_HELP
+    number_of_rows = 8
+    number_of_cols = 2
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
-    modules = [custom.Button.inline(
-        "{} {} {}".format(random.choice(list(multi)), x, random.choice(list(multi))),
-        data="us_plugin_{}".format(x))
-        for x in helpable_plugins]
-    if number_of_cols == 1:
-        pairs = list(zip(modules[::number_of_cols]))
-    elif number_of_cols == 2:
-        pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
-    else:
-        pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols], modules[2::number_of_cols]))
+    modules = [
+        custom.Button.inline(
+            "{} {} {}".format("✨", x, "✨"), data="us_plugin_{}".format(x)
+        )
+        for x in helpable_plugins
+    ]
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
-    elif len(modules) % number_of_cols == 2:
-        pairs.append((modules[-2],modules[-1]))
     max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
-        pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
-            [
-            (custom.Button.inline("Previous", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("Next", data="{}_next({})".format(prefix, modulo_page)))
+        pairs = pairs[
+            modulo_page * number_of_rows : number_of_rows * (modulo_page + 1)
+        ] + [
+            (
+                custom.Button.inline(
+                    "Previous", data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                custom.Button.inline(
+                    "Next", data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
         ]
     return pairs
