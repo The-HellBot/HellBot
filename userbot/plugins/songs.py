632720d2375354903a8d48733d5c07f4e12bb719
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-10-17 00:39:39 +0530

    Update songs.py

--- userbot/plugins/songs.py
+++ userbot/plugins/songs.py
@@ -1,6 +1,7 @@
 
 # Port to UserBot
 # modified by @hellboi_atul
+# extra mod by @kraken_the_badass
 # Copyright (C) 2020.
 
 from telethon import events
@@ -46,35 +47,29 @@ async def _(event):
           await event.delete()
           await bot.forward_messages(event.chat_id, respond.message)
 
-@register(outgoing=True, pattern="^.music(?: |$)(.*)")
-async def WooMai(netase):
-    if netase.fwd_from:
+@register(outgoing=True, pattern="^.getsong(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
         return
-    song = netase.pattern_match.group(1)
-    chat = "@WooMaiBot"
-    link = f"/netease {song}"
-    await netase.edit("```😉Getting Your Music🎶```")
+    link = event.pattern_match.group(1)
+    chat = "@MissAlexaRobot"
+    await event.edit("```🎶Getting Your Music🎶```")
     async with bot.conversation(chat) as conv:
           await asyncio.sleep(2)
-          await netase.edit("🎵`Downloading...Please wait😉`")
+          await event.edit("`🎵Downloading music taking some times,  Stay Tuned.....🔥😉`")
           try:
-              msg = await conv.send_message(link)
-              response = await conv.get_response()
-              respond = await conv.get_response()
-              """ - don't spam notif - """
-              await bot.send_read_acknowledge(conv.chat_id)
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1361631434))
+              await bot.send_message(chat, link)
+              respond = await response
           except YouBlockedUserError:
-              await netase.edit("```😐Please unblock @WooMaiBot and try again```")
+              await event.reply("```😐Please unblock @MissAlexaRobot and try again```")
               return
-          await netase.edit("`🔥Sending Your Music...weit!😎`")
-          await asyncio.sleep(3)
-          await bot.send_file(netase.chat_id, respond)
-    await netase.client.delete_messages(conv.chat_id,
-                                       [msg.id, response.id, respond.id])
-    await netase.delete()
+          await event.delete()
+          await bot.forward_messages(event.chat_id, respond.message)
 
 
-@register(outgoing=True, pattern="^.getsong(?: |$)(.*)")
+
+@register(outgoing=True, pattern="^.dwlsong(?: |$)(.*)")
 async def DeezLoader(Deezlod):
     if Deezlod.fwd_from:
         return
@@ -106,9 +101,9 @@ CMD_HELP.update({
         "music":
         ".song`<Artist - Song Title>\
             \nUsage:For searching songs from Spotify.\
-            \n\n`.gmusic` <Artist - Song Title>\
-            \nUsage:Download music with @WooMaiBot\
-            \n\n`.getsong` <Spotify/Deezer Link>\
+            \n\n`.getsong` <Artist - Song Title>\
+            \nUsage:Download music with @MissAlexaRobot\
+            \n\n`.dwlsong` <Spotify/Deezer Link>\
             \nUsage:Download music from Spotify or Deezer.\
             \n\n`.deezload` <spotify/deezer link> <Format>\
             \nUsage: Download music from deezer.\
