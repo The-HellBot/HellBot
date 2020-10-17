f6d7c362f9efb9f9146a51c42842c041af517199
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-10-17 09:46:59 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -406,9 +406,8 @@ def sudo_cmd(pattern=None, **args):
             args["pattern"] = re.compile(pattern)
         else:
             
-            args["pattern"] = re.compile(Config.SUDO_COMMAND_HAND_LER + pattern)
-            reg =Config.SUDO_COMMAND_HAND_LER[1]
-            cmd = (reg +pattern).replace("$", "").replace("\\", "").replace("^", "")
+            args["pattern"] = re.compile(cmdhandler + pattern)
+            cmd = cmdhandler + pattern
             try:
                 SUDO_LIST[file_test].append(cmd)
             except:
