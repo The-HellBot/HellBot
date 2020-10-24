442ca7fb80bf798644ce22d6b0d0fd95c18cf81c
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-10-24 20:01:06 +0530

    Credits to userge....

---
+++ userbot/plugins/adultzone.py
@@ -0,0 +1,36 @@
+#credits to userge
+#ported to Hellbot by @kraken_the_badass
+#will be adding more soon
+
+import os, urllib, requests, asyncio
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd(pattern=r"boobs"))
+async def boobs(event):
+    if not os.path.isdir(Var.TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Var.TEMP_DOWNLOAD_DIRECTORY)
+    pic_loc = os.path.join(Var.TEMP_DOWNLOAD_DIRECTORY, "bobs.jpg")
+    a = await event.reply("Finding some big boobs for u 🧐")
+    await asyncio.sleep(0.5)
+    await a.edit("Sending some big boobs🤪")
+    nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), pic_loc)
+    await event.client.send_file(event.chat_id, pic_loc, force_document=False)
+    os.remove(pic_loc)
+    await event.delete()
+    await a.delete()
+
+@borg.on(admin_cmd(pattern=r"butts"))
+async def butts(event):
+    if not os.path.isdir(Var.TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Var.TEMP_DOWNLOAD_DIRECTORY)
+    pic_loc = os.path.join(Var.TEMP_DOWNLOAD_DIRECTORY, "butts.jpg")
+    a = await event.reply("Finding some beautiful butts for u🧐")
+    await asyncio.sleep(0.5)
+    await a.edit("Sending some beautiful butts🤪")
+    nsfw = requests.get('http://api.obutts.ru/noise/1').json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.obutts.ru/{}".format(nsfw), pic_loc)
+    await event.client.send_file(event.chat_id, pic_loc, force_document=False)
+    os.remove(pic_loc)
+    await event.delete()
+    await a.delete()
