fe22d1565de90ba6b35626c8b8c0fd15b167957f
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-04 14:24:59 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -56,7 +56,7 @@ async def install(event):
                 shortname = path1.stem
                 load_module(shortname.replace(".py", ""))
                 await event.edit(
-                    "Plugin successfully installed\n [{DEFAULTUSER}](tg://user?id={kraken})\n\n `{}`".format(
+                    "Plugin successfully installed\n `{}`".format(
                         os.path.basename(downloaded_file_name)
                     )
                 )
