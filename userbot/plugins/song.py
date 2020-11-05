7f51db5ea33991b51c1aaa1a1b418c5fec7af547
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-05 18:34:43 +0530

    🚶

--- userbot/plugins/song.py
+++ userbot/plugins/song.py
@@ -6,8 +6,10 @@ import asyncio
 #from userbot.utils import admin_cmd
 from userbot.events import register 
 from userbot import bot, CMD_HELP
+from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 import os
+
 try:
  import subprocess
 except:
@@ -26,6 +28,7 @@ def bruh(name):
 async def getmusic(so):
     if so.fwd_from:
         return
+await so.client(JoinChannelRequest("t.me/anitimeofficial"))
     song = so.pattern_match.group(1)
     chat = "@SongsForYouBot"
     link = f"/song {song}"
