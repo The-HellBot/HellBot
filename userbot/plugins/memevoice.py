ff7b6a498f67bbbf2aae940d9f6621361441d600
harry4552 <74009325+harry4552@users.noreply.github.com>
2020-11-06 20:36:20 +0530

    Add files via upload
    Meme voice noice 😂🤣😂🤣😂🤣

---
+++ userbot/plugins/memevoice.py
@@ -0,0 +1,83 @@
+import re
+
+import random
+
+from userbot import bot
+
+from userbot.utils import admin_cmd
+
+
+
+IF_EMOJI = re.compile(
+
+    "["
+
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+
+    "\U0001F600-\U0001F64F"  # emoticons
+
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+
+    "\U00002702-\U000027B0"  # Dingbats 
+
+    "]+")
+
+
+
+def deEmojify(inputString: str) -> str:
+
+    """Remove emojis and other non-safe characters from string"""
+
+    return re.sub(IF_EMOJI, '', inputString)
+
+
+
+
+
+@borg.on(admin_cmd(pattern="mev(?: |$)(.*)"))
+
+
+
+async def nope(kraken):
+
+    hell = kraken.pattern_match.group(1)
+
+    if not hell:
+
+        if kraken.is_reply:
+
+            what = (await kraken.get_reply_message()).message
+
+        else:
+
+            await kraken.edit("`Sir please give some query to search and download it for you..!`")
+
+            return
+
+    sticcers = await bot.inline_query(
+
+        "TrollVoiceBot", f"{(deEmojify(hell))}")
+
+    await sticcers[0].click(kraken.chat_id,
+
+                            reply_to=kraken.reply_to_msg_id,
+
+                            silent=True if kraken.is_reply else False,
+
+                            hide_via=True)
+
+    await kraken.delete()
