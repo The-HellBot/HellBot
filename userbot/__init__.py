27d2986a56954e1bfd17e6aefae51487718a5396
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-07 20:30:45 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -4,7 +4,7 @@ import sys
 import time
 from telethon.sessions import StringSession
 from telethon import TelegramClient
-from userbot import functions as simpdef
+from userbot.helpers import functions as simpdef
 
 from var import Var
 
