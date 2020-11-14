0e7c423614b51423903840ee53bb2b8a8615ebcb
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-14 12:56:22 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,23 +5,21 @@ from userbot import ALIVE_NAME, hellversion
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
 PM_IMG = "https://telegra.ph/file/f34675b4e94d4290c0b6b.mp4"
-pm_caption = "🔥🔥**Hêllẞø† IS ONLINE🔥🔥\n\n\n"
+pm_caption = "🔥🔥**Hêllẞø† IS ONLINE**🔥🔥\n\n"
 
-pm_caption += f"⚔️⚔️**MASTER**⚔️⚔️       : {DEFAULTUSER}\n\n"
+pm_caption += f"⚔️⚔️MASTER⚔️⚔️       : {DEFAULTUSER}\n"
 
-pm_caption += "🛡️🛡️**TELETHON**🛡️🛡️   : 1.15.0 \n\n"
+pm_caption += "🛡️🛡️TELETHON🛡️🛡️   : 1.15.0 \n"
 
-pm_caption += f"😈😈**Hêllẞø†**😈😈         : `{hellversion}`\n\n"
+pm_caption += f"😈😈Hêllẞø†😈😈         : `{hellversion}`\n"
 
-pm_caption += "⚠️⚠️**CHANNEL**⚠️⚠️     : [ᴊᴏɪɴ](https://t.me/HellBot_Official)\n\n"
+pm_caption += "⚠️⚠️CHANNEL⚠️⚠️     : [ᴊᴏɪɴ](https://t.me/HellBot_Official)\n"
 
-pm_caption += "🔱🔱**GROUP**🔱🔱.         : [ᴊᴏɪɴ](https://t.me/HellBot_Official_Chat)\n\n"
+pm_caption += "🔱🔱GROUP🔱🔱.         : [ᴊᴏɪɴ](https://t.me/HellBot_Official_Chat)\n"
 
-pm_caption += "😎😎**LICENSE**😎😎       : [ӀíϲҽղՏҽ](https://github.com/HellBoy-OP/HellBot/blob/master/LICENSE)\n\n"
+pm_caption += "😎😎LICENSE😎😎       : [ӀíϲҽղՏҽ](https://github.com/HellBoy-OP/HellBot/blob/master/LICENSE)\n"
 
-pm_caption += "🔥🔥**CREATOR🔥🔥      : [HellBot-Owner](https://t.me/kraken_the_badass)\n\n"
-
-pm_caption += " [...▄███▄███▄\n....█████████\n.......▀█████▀\n............▀█▀\n](https://t.me/hellbot_official)\n\n"
+pm_caption += "🔥🔥CREATOR🔥🔥      : [Nub Here](https://t.me/kraken_the_badass)\n"
 #@command(outgoing=True, pattern="^.alive$")
 @bot.on(admin_cmd(outgoing=True, pattern="alive$"))
 @bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
