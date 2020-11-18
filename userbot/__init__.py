c01ff786f8c73077ec421cac3bede0b8b96d7b74
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-18 18:52:41 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -9,7 +9,7 @@ from userbot.helpers import functions as simpdef
 from var import Var
 
 StartTime = time.time()
-hellversion = "𝟙.𝟞" 
+hellversion = "1.7" 
 
 os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
