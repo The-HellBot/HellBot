1bc228d49f89ab495a35cc030fc1556461a2608d
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-23 14:51:28 +0530

    Read additional description........
    Tujhe use karna h to use kar.....
    Tujhe chori karna h to chori kar....
    Magar ek baat sunn le.....
    Mere gand me ungli matt kar.....
    Warna pura lakda dal denga mai.....
    Samjha madarchod

--- userbot/plugins/autodp.py
+++ userbot/plugins/autodp.py
@@ -180,7 +180,7 @@ async def autopic(event):
             ))
             os.remove(photo)
             counter -= 30
-            await asyncio.sleep(60)
+            await asyncio.sleep(600)
         except:
             return
 
@@ -386,7 +386,7 @@ async def autopic(event):
 
             os.remove(photo)
 
-            await asyncio.sleep(60)
+            await asyncio.sleep(600)
 
         except:
 
@@ -457,7 +457,7 @@ async def main(event):
 
         os.system("rm -rf donottouch.jpg")
 
-        await asyncio.sleep(300) #Edit this to your required needs
+        await asyncio.sleep(600) #Edit this to your required needs
 
 #-------------------------------------------------------------------------------
 
@@ -514,7 +514,7 @@ async def main(event):
 
         os.system("rm -rf donottouch.jpg")
 
-        await asyncio.sleep(60) #Edit this to your required needs
+        await asyncio.sleep(600) #Edit this to your required needs
 
 #-------------------------------------------------------------------------------
 
@@ -623,7 +623,7 @@ async def main(event):
 
         os.system("rm -rf donottouch.jpg")
 
-        await asyncio.sleep(60) #Edit this to your required needs
+        await asyncio.sleep(600) #Edit this to your required needs
 
 #-------------------------------------------------------------------------------
 
@@ -663,7 +663,7 @@ async def main(event):
         await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
         await event.client(functions.photos.UploadProfilePhotoRequest( file))
         os.system("rm -rf donottouch.jpg")
-        await asyncio.sleep(120) #Edit this to your required needs
+        await asyncio.sleep(600) #Edit this to your required needs
 
 #-------------------------------------------------------------------------------
 
@@ -724,7 +724,7 @@ async def main(event):
 
         os.system("rm -rf donottouch.jpg")
 
-        await asyncio.sleep(3600) #Edit this to your required needs
+        await asyncio.sleep(600) #Edit this to your required needs
 
 #-------------------------------------------------------------------------------
 
@@ -785,9 +785,78 @@ async def autopic(event):
             ))
             os.remove(photo)
             
-            await asyncio.sleep(30)
+            await asyncio.sleep(600)
         except:
             return
           
 #-------------------------------------------------------------------------------
 
+import requests , re , random 
+import urllib , os 
+from telethon.tl import functions
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from userbot.utils import admin_cmd
+import asyncio
+from time import sleep
+
+AUTOPFP_PACK = os.environ.get("AUTOPFP_PACK", None)
+if AUTOPFP_PACK is None:
+  PACK = [
+  "Epic-Space-Wallpaper",
+   "Acoustic-Guitar-Wallpaper-HD",
+   "Taylor-Guitar-Wallpaper",
+   "Classical-Music-Wallpapers-for-Desktop",
+   "Prs-Guitar-Wallpaper",
+   "Iron-Man-Wallpaper-1920x1080",
+   "Dodge-Challenger-Black-Hellcat-Wallpaper",
+   "V-for-Vendetta-Mask-Wallpaper",
+   "Toxic-Mask-Wallpapers",
+   "Minion-Desktop-Wallpaper",
+   "Epic-1080p-Wallpapers",
+   "Japanese-Desktop-Wallpaper",
+   "Cool-Gold-Cars-Wallpapers",
+   "Pretty-Wallpapers-for-iPhone-Quotes",
+   "dark-abstract-wallpaper",
+   "abstract-dark-wallpaper",
+   "abstract-wallpapers-and-screensavers",
+   "roaring-lion-wallpaper",
+   "wolves-screensavers-and-wallpaper",
+   "cool-wallpaper-for-men",
+   "Epic-1080p-Wallpapers",
+   "hacker-background",
+   "Vietnam-War-Wallpapers",
+   "War-of-the-Worlds-Wallpaper",
+   "War-Plane-Wallpaper",
+   "World-War-Ii-Wallpaper",
+   "Cool-War-Wallpapers",
+   "World-War-2-Wallpaper-HD"
+
+  ]
+else:
+  PACK = AUTOPFP_PACK
+async def animepp():
+    os.system("rm -rf donot.jpg")
+    rnd = random.randint(0, len(PACK) - 1)
+    pack = PACK[rnd]
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+    f = re.compile('/\w+/full.+.jpg')
+    f = f.findall(pc)
+    fy = "http://getwallpapers.com"+random.choice(f)
+    print(fy)
+    if not os.path.exists("f.ttf"):
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+@borg.on(admin_cmd(pattern="wallpapers ?(.*)"))
+async def main(event):
+    await event.edit("Activated Wallpapers On Your DP\n Enjoy💜") 
+    while True:
+      try:
+        await animepp()
+        file = await event.client.upload_file("donottouch.jpg")
+        await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+        os.system("rm -rf donottouch.jpg")
+      except:
+        pass
+      await asyncio.sleep(600)
