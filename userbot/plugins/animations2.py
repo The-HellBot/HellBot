41a19feb8b24dba09e939e022bdee4f511bcb997
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-24 14:49:37 +0530

    Create animations2.py

---
+++ userbot/plugins/animations2.py
@@ -0,0 +1,306 @@
+import asyncio
+from collections import deque
+
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot import ALIVE_NAME, CMD_HELP
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
+
+
+@bot.on(admin_cmd(pattern=r"boxs$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"boxs$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "`boxs...`")
+    deq = deque(list("🟥🟧🟨🟩🟦🟪🟫⬛⬜"))
+    for _ in range(999):
+        await asyncio.sleep(0.3)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(pattern=r"rain$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"rain$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "`Raining.......`")
+    deq = deque(list("🌬☁️🌩🌨🌧🌦🌥⛅🌤"))
+    for _ in range(48):
+        await asyncio.sleep(0.3)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@bot.on(admin_cmd(pattern=r"deploy$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"deploy$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 3
+    animation_ttl = range(12)
+    event = await edit_or_reply(event, "`Deploying...`")
+    animation_chars = [
+     "**Heroku Connecting To Latest [Github Build](HellBoy-OP/HellBot)**",
+            f"**Build started by user** {DEFAULTUSER}",
+            f"**Deploy** `535a74f0` **by user** **{DEFAULTUSER}**",
+            "**Restarting Heroku Server...**",
+            "**State changed from up to starting**",    
+            "**Stopping all processes with SIGTERM**",
+            "**Process exited with** `status 143`",
+            "**Starting process with command** `python3 -m stdborg`",
+            "**State changed from starting to up**",
+            "__INFO:Hêllẞø†:Logged in as 557667062__",
+            "__INFO:Hêllẞø†:Successfully loaded all plugins__",
+            "**Build Succeeded**",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 12])
+
+
+@bot.on(admin_cmd(pattern=r"dump$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"dump$", allow_sudo=True))
+async def _(message):
+    if event.fwd_from:
+        return
+    try:
+        obj = message.pattern_match.group(1)
+        if len(obj) != 3:
+            raise IndexError
+        inp = " ".join(obj)
+    except IndexError:
+        inp = "🥞 🎂 🍫"
+    event = await edit_or_reply(message, "`droping....`")
+    u, t, g, o, s, n = inp.split(), "🗑", "<(^_^ <)", "(> ^_^)>", "⠀ ", "\n"
+    h = [(u[0], u[1], u[2]), (u[0], u[1], ""), (u[0], "", "")]
+    for something in reversed(
+        [
+            y
+            for y in (
+                [
+                    "".join(x)
+                    for x in (
+                        f + (s, g, s + s * f.count(""), t),
+                        f + (g, s * 2 + s * f.count(""), t),
+                        f[:i] + (o, f[i], s * 2 + s * f.count(""), t),
+                        f[:i] + (s + s * f.count(""), o, f[i], s, t),
+                        f[:i] + (s * 2 + s * f.count(""), o, f[i], t),
+                        f[:i] + (s * 3 + s * f.count(""), o, t),
+                        f[:i] + (s * 3 + s * f.count(""), g, t),
+                    )
+                ]
+                for i, f in enumerate(reversed(h))
+            )
+        ]
+    ):
+        for something_else in something:
+            await asyncio.sleep(0.3)
+            try:
+                await event.edit(something_else)
+            except errors.MessageIdInvalidError:
+                return
+
+
+@bot.on(admin_cmd(pattern=r"fleaveme$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"fleaveme$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1
+    animation_ttl = range(10)
+    animation_chars = [
+        "⬛⬛⬛\n⬛⬛⬛\n⬛⬛⬛",
+        "⬛⬛⬛\n⬛🔄⬛\n⬛⬛⬛",
+        "⬛⬆️⬛\n⬛🔄⬛\n⬛⬛⬛",
+        "⬛⬆️↗️\n⬛🔄⬛\n⬛⬛⬛",
+        "⬛⬆️↗️\n⬛🔄➡️\n⬛⬛⬛",
+        "⬛⬆️↗️\n⬛🔄➡️\n⬛⬛↘️",
+        "⬛⬆️↗️\n⬛🔄➡️\n⬛⬇️↘️",
+        "⬛⬆️↗️\n⬛🔄➡️\n↙️⬇️↘️",
+        "⬛⬆️↗️\n⬅️🔄➡️\n↙️⬇️↘️",
+        "↖️⬆️↗️\n⬅️🔄➡️\n↙️⬇️↘️",
+    ]
+    event = await edit_or_reply(event, "fleaveme....")
+    await asyncio.sleep(2)
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 10])
+
+
+@bot.on(admin_cmd(pattern=r"loveu$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"loveu$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.5
+    animation_ttl = range(70)
+    event = await edit_or_reply(event, "loveu")
+    animation_chars = [
+        "😀",
+        "👩‍🎨",
+        "😁",
+        "😂",
+        "🤣",
+        "😃",
+        "😄",
+        "😅",
+        "😊",
+        "☺",
+        "🙂",
+        "🤔",
+        "🤨",
+        "😐",
+        "😑",
+        "😶",
+        "😣",
+        "😥",
+        "😮",
+        "🤐",
+        "😯",
+        "😴",
+        "😔",
+        "😕",
+        "☹",
+        "🙁",
+        "😖",
+        "😞",
+        "😟",
+        "😢",
+        "😭",
+        "🤯",
+        "💔",
+        "❤",
+        "I Love You❤",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 35])
+
+
+@bot.on(admin_cmd(pattern=r"plane$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"plane$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    event = await edit_or_reply(event, "Wait for plane...")
+    await event.edit("✈-------------")
+    await event.edit("-✈------------")
+    await event.edit("--✈-----------")
+    await event.edit("---✈----------")
+    await event.edit("----✈---------")
+    await event.edit("-----✈--------")
+    await event.edit("------✈-------")
+    await event.edit("-------✈------")
+    await event.edit("--------✈-----")
+    await event.edit("---------✈----")
+    await event.edit("----------✈---")
+    await event.edit("-----------✈--")
+    await event.edit("------------✈-")
+    await event.edit("-------------✈")
+    await asyncio.sleep(3)
+
+
+@bot.on(admin_cmd(pattern=r"police$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"police$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(12)
+    event = await edit_or_reply(event, "Police")
+    animation_chars = [
+        "🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵",
+        "🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴",
+        "🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵",
+        "🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴",
+        "🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵",
+        "🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴",
+        "🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵",
+        "🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴",
+        "🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵",
+        "🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴\n🔵🔵🔵⬜⬜⬜🔴🔴🔴",
+        "🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵\n🔴🔴🔴⬜⬜⬜🔵🔵🔵",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 12])
+
+
+@bot.on(admin_cmd(pattern=r"jio$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"jio$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1
+    animation_ttl = range(19)
+    event = await edit_or_reply(event, "jio network boosting...")
+    animation_chars = [
+        "`Connecting To JIO NETWORK ....`",
+        "`█ ▇ ▆ ▅ ▄ ▂ ▁`",
+        "`▒ ▇ ▆ ▅ ▄ ▂ ▁`",
+        "`▒ ▒ ▆ ▅ ▄ ▂ ▁`",
+        "`▒ ▒ ▒ ▅ ▄ ▂ ▁`",
+        "`▒ ▒ ▒ ▒ ▄ ▂ ▁`",
+        "`▒ ▒ ▒ ▒ ▒ ▂ ▁`",
+        "`▒ ▒ ▒ ▒ ▒ ▒ ▁`",
+        "`▒ ▒ ▒ ▒ ▒ ▒ ▒`",
+        "*Optimising JIO NETWORK...*",
+        "`▒ ▒ ▒ ▒ ▒ ▒ ▒`",
+        "`▁ ▒ ▒ ▒ ▒ ▒ ▒`",
+        "`▁ ▂ ▒ ▒ ▒ ▒ ▒`",
+        "`▁ ▂ ▄ ▒ ▒ ▒ ▒`",
+        "`▁ ▂ ▄ ▅ ▒ ▒ ▒`",
+        "`▁ ▂ ▄ ▅ ▆ ▒ ▒`",
+        "`▁ ▂ ▄ ▅ ▆ ▇ ▒`",
+        "`▁ ▂ ▄ ▅ ▆ ▇ █`",
+        "**JIO NETWORK Boosted....**",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 19])
+
+
+@bot.on(admin_cmd(pattern=r"solarsystem$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"solarsystem$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.1
+    animation_ttl = range(80)
+    event = await edit_or_reply(event, "solarsystem")
+    animation_chars = [
+        "`◼️◼️◼️◼️◼️\n◼️◼️◼️◼️☀\n◼️◼️🌎◼️◼️\n🌕◼️◼️◼️◼️\n◼️◼️◼️◼️◼️`",
+        "`◼️◼️◼️◼️◼️\n🌕◼️◼️◼️◼️\n◼️◼️🌎◼️◼️\n◼️◼️◼️◼️☀\n◼️◼️◼️◼️◼️`",
+        "`◼️🌕◼️◼️◼️\n◼️◼️◼️◼️◼️\n◼️◼️🌎◼️◼️\n◼️◼️◼️◼️◼️\n◼️◼️◼️☀◼️`",
+        "`◼️◼️◼️🌕◼️\n◼️◼️◼️◼️◼️\n◼️◼️🌎◼️◼️\n◼️◼️◼️◼️◼️\n◼️☀◼️◼️◼️`",
+        "`◼️◼️◼️◼️◼️\n◼️◼️◼️◼️🌕\n◼️◼️🌎◼️◼️\n☀◼️◼️◼️◼️\n◼️◼️◼️◼️◼️`",
+        "`◼️◼️◼️◼️◼️\n☀◼️◼️◼️◼️\n◼️◼️🌎◼️◼️\n◼️◼️◼️◼️🌕\n◼️◼️◼️◼️◼️`",
+        "`◼️☀◼️◼️◼️\n◼️◼️◼️◼️◼️\n◼️◼️🌎◼️◼️\n◼️◼️◼️◼️◼️\n◼️◼️◼️🌕◼️`",
+        "`◼️◼️◼️☀◼️\n◼️◼️◼️◼️◼️\n◼️◼️🌎◼️◼️\n◼️◼️◼️◼️◼️\n◼️🌕◼️◼️◼️`",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 8])
+
+
+CMD_HELP.update(
+    {
+        "animation2": """**Plugin : **`animation2`
+        
+**Commands in animation2 are **
+  •  `.boxs`
+  •  `.rain`
+  •  `.deploy`
+  •  `.dump`
+  •  `.fleaveme`
+  •  `.loveu`
+  •  `.plane`
+  •  `.police`
+  •  `.jio`
+  •  `.solarsystem`
+  
+**Function : **__Different kinds of animation commands check yourself for their animation .__"""
+    }
+)
