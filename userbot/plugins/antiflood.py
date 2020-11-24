bd1e2d7f551d60edf9a1902a928bd83baa683951
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-24 12:52:49 +0530

    Update antiflood.py

--- userbot/plugins/antiflood.py
+++ userbot/plugins/antiflood.py
@@ -1,81 +1,81 @@
-# Idhar kya dekhne aaya be bsdk. Credit to @Kraken_The_BadASS for @HellBot_official
+
 
 import asyncio
-from telethon import events
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights
-from userbot.utils import admin_cmd
 import userbot.plugins.sql_helper.antiflood_sql as sql
-import userbot.utils
-from userbot.utils import humanbytes, progress, time_formatter
 from userbot import CMD_HELP
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 CHAT_FLOOD = sql.__load_flood_settings()
 # warn mode for anti flood
 ANTI_FLOOD_WARN_MODE = ChatBannedRights(
-    until_date=None,
-    view_messages=None,
-    send_messages=True
+    until_date=None, view_messages=None, send_messages=True
 )
 
 
-@borg.on(admin_cmd(incoming=True))
+@bot.on(admin_cmd(incoming=True))
 async def _(event):
-    # logger.info(CHAT_FLOOD)
     if not CHAT_FLOOD:
         return
-    if not (str(event.chat_id) in CHAT_FLOOD):
+    if str(event.chat_id) not in CHAT_FLOOD:
         return
-    # TODO: exempt admins from this
-    should_ban = sql.update_flood(event.chat_id, event.message.from_id)
+    should_ban = sql.update_flood(event.chat_id, event.message.sender_id)
     if not should_ban:
         return
     try:
-        await event.client(EditBannedRequest(
-            event.chat_id,
-            event.message.from_id,
-            ANTI_FLOOD_WARN_MODE
-        ))
+        await event.client(
+            EditBannedRequest(
+                event.chat_id, event.message.sender_id, ANTI_FLOOD_WARN_MODE
+            )
+        )
     except Exception as e:  # pylint:disable=C0103,W0703
         no_admin_privilege_message = await event.client.send_message(
             entity=event.chat_id,
             message="""**Automatic AntiFlooder**
 @admin [User](tg://user?id={}) is flooding this chat.
-
-`{}`""".format(event.message.from_id, str(e)),
-            reply_to=event.message.id
+`{}`""".format(
+                event.message.sender_id, str(e)
+            ),
+            reply_to=event.message.id,
         )
         await asyncio.sleep(10)
         await no_admin_privilege_message.edit(
-            "**Bruh, This is useless spam... Stop it njoy tha chat🚶** ",
-            link_preview=False
+            "This is useless SPAM dude. Stop this, enjoy chat man ", link_preview=False
         )
     else:
         await event.client.send_message(
             entity=event.chat_id,
             message="""**Automatic AntiFlooder**
 [User](tg://user?id={}) has been automatically restricted
-because he reached the defined flood limit.""".format(event.message.from_id),
-            reply_to=event.message.id
+because he reached the defined flood limit.""".format(
+                event.message.sender_id
+            ),
+            reply_to=event.message.id,
         )
 
 
-@borg.on(admin_cmd(pattern="setflood (.*)"))
+@bot.on(admin_cmd(pattern="setflood(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="setflood(?: |$)(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
+    event = await edit_or_reply(event, "updating flood settings!")
     try:
         sql.set_flood(event.chat_id, input_str)
-        CHAT_FLOOD = sql.__load_flood_settings()
-        await event.edit("Antiflood updated to {} in the current chat".format(input_str))
+        sql.__load_flood_settings()
+        await event.edit(
+            "Antiflood updated to {} in the current chat".format(input_str)
+        )
     except Exception as e:  # pylint:disable=C0103,W0703
         await event.edit(str(e))
 
-        
-CMD_HELP.update({
-    "antiflood":
-    ".setflood [number]\
-\nUsage: warns the user if he spams the chat  if you are admin mutes him in that group .\
+
+CMD_HELP.update(
+    {
+        "antiflood": ".setflood [number]\
+\nUsage: warns the user if he spams the chat and if you are an admin then it mutes him in that group.\
 "
-})              
+    }
+)
