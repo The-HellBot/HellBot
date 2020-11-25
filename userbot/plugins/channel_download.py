2af18d255018a6bf5b0ea5b4856629009729ec81
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-25 09:15:06 +0530

    Update channel_download.py

--- userbot/plugins/channel_download.py
+++ userbot/plugins/channel_download.py
@@ -1,82 +1,94 @@
 """
 Telegram Channel Media Downloader Plugin for userbot.
 usage: .geta channel_username [will  get all media from channel, tho there is limit of 3000 there to prevent API limits.]
-       .getc number_of_messsages channel_username  
+       .getc number_of_messsages channel_username
 By: @Zero_cool7870
 """
-from telethon import events
-import asyncio
 import os
 import subprocess
-import sys
-from uniborg.util import admin_cmd, humanbytes, progress, time_formatter
 
-@borg.on(admin_cmd(pattern=r"getc"))
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+
+location = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "temp")
+
+
+@bot.on(admin_cmd(pattern=r"getc(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="getc(?: |$)(.*)", allow_sudo=True))
 async def get_media(event):
     if event.fwd_from:
         return
-    dir= "./temp/"
+    tempdir = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "temp")
     try:
-        os.makedirs("./temp/")
-    except:
-    	pass
-    channel_username= event.text
-    command = ['ls','temp','|','wc','-l' ]
-    limit = channel_username[6:9]
-    print(limit)
-    channel_username = channel_username[11: ]
-    print(channel_username)
-    await event.edit("Downloading Media From this Channel.")
-    msgs = await borg.get_messages(channel_username, limit=int(limit))
-    with open('log.txt','w') as f:
-    	f.write(str(msgs))
+        os.makedirs(tempdir)
+    except BaseException:
+        pass
+    hell = event.pattern_match.group(1)
+    limit = int(hell.split(" ")[0])
+    channel_username = str(hell.split(" ")[1])
+    event = await edit_or_reply(event, "Downloading Media From this Channel.")
+    msgs = await event.client.get_messages(channel_username, limit=int(limit))
+    with open("log.txt", "w") as f:
+        f.write(str(msgs))
+    i = 0
     for msg in msgs:
-       if msg.media is not None:
-	        await borg.download_media(
-                msg,dir)
-    ps = subprocess.Popen(('ls', 'temp'), stdout=subprocess.PIPE)
-    output = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
+        if msg.media is not None:
+            await event.client.download_media(msg, tempdir)
+            i += 1
+            await event.edit(
+                f"Downloading Media From this Channel.\n **DOWNLOADED : **`{i}`"
+            )
+    ps = subprocess.Popen(("ls", tempdir), stdout=subprocess.PIPE)
+    output = subprocess.check_output(("wc", "-l"), stdin=ps.stdout)
     ps.wait()
     output = str(output)
-    output = output.replace("b'","")
-    output = output.replace("\n'","")
-    await event.edit("Downloaded "+output+" files.")
-             
-             
-             
-             
-             
-             
-@borg.on(admin_cmd(pattern=r"geta"))
+    output = output.replace("b'", " ")
+    output = output.replace("\\n'", " ")
+    await event.edit(f"Successfully downloaded {output} number of media files")
+
+
+@bot.on(admin_cmd(pattern="geta(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="geta(?: |$)(.*)", allow_sudo=True))
 async def get_media(event):
     if event.fwd_from:
         return
-    dir= "./temp/"
+    tempdir = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "temp")
     try:
-        os.makedirs("./temp/")
-    except:
-    	pass
-    channel_username= event.text
-    command = ['ls','temp','|','wc','-l' ]
-    channel_username = channel_username[7:]
- 
-   
-    print(channel_username)
-    await event.edit("Downloading All Media From this Channel.")
-    msgs = await borg.get_messages(channel_username,limit=3000)
-    with open('log.txt','w') as f:
-    	f.write(str(msgs))
+        os.makedirs(tempdir)
+    except BaseException:
+        pass
+    channel_username = event.pattern_match.group(1)
+    event = await edit_or_reply(event, "Downloading All Media From this Channel.")
+    msgs = await event.client.get_messages(channel_username, limit=3000)
+    with open("log.txt", "w") as f:
+        f.write(str(msgs))
+    i = 0
     for msg in msgs:
-       if msg.media is not None:
-	        await borg.download_media(
-                msg,dir)          
-    ps = subprocess.Popen(('ls', 'temp'), stdout=subprocess.PIPE)
-    output = subprocess.check_output(('wc', '-l'), stdin=ps.stdout)
+        if msg.media is not None:
+            await event.client.download_media(msg, tempdir)
+            i += 1
+            await event.edit(
+                f"Downloading Media From this Channel.\n **DOWNLOADED : **`{i}`"
+            )
+    ps = subprocess.Popen(("ls", tempdir), stdout=subprocess.PIPE)
+    output = subprocess.check_output(("wc", "-l"), stdin=ps.stdout)
     ps.wait()
     output = str(output)
-    output = output.replace("b'","")
-    output = output.replace("\n'","")
-    await event.edit("Downloaded "+output+" files.")
-             
-             
-             
+    output = output.replace("b'", "")
+    output = output.replace("\\n'", "")
+    await event.edit(f"Successfully downloaded {output} number of media files")
+
+
+CMD_HELP.update(
+    {
+        "channel_download": f"""**Plugin : **`channel_download`
+**Telegram Channel Media Downloader Plugin for userbot.**
+  • **Syntax : **`.geta channel_username` 
+  • **Function : **__will  download all media from channel into your bot server but there is limit of 3000 to prevent API limits.__
+  
+  • **Syntax : **`.getc number channel_username` 
+  • **Function : **__will  download latest given number of media from channel into your bot server .__
+  
+**Note : **__The downloaded media files will be at__ `.ls {location}`"""
+    }
+)
