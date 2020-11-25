b201c9116cb2ae510935370b0b283fdb91cf4fdf
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-25 20:28:48 +0530

    Will add sudo soon

--- userbot/plugins/cow.py
+++ userbot/plugins/cow.py
@@ -6,7 +6,7 @@ and may cmd would be added soon."""
 import asyncio
 from telethon import events
 from cowpy import cow
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @borg.on(events.NewMessage(pattern=r"^.(\w+)say (.*)", outgoing=True))
