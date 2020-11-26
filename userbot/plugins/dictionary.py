8ba9f85e23b2c91bed63390d0435e4ece2f432c6
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-26 08:36:38 +0530

    Update dictionary.py

--- userbot/plugins/dictionary.py
+++ userbot/plugins/dictionary.py
@@ -1,44 +1,60 @@
-"""Dictionary Plugin for @UniBorg
-Syntax: .meaning <word>"""
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""Urban Dictionary
+Syntax: .ud Query"""
+import asyncurban
+from PyDictionary import PyDictionary
 
-import requests
-from telethon import events
-from uniborg.util import admin_cmd
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
-@borg.on(admin_cmd("meaning (.*)"))
+@bot.on(admin_cmd(pattern="ud (.*)"))
+@bot.on(sudo_cmd(pattern="ud (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    input_str = event.pattern_match.group(1)
-    input_url = "https://bots.shrimadhavuk.me/dictionary/?s={}".format(input_str)
-    headers = {"USER-AGENT": "UniBorg"}
-    caption_str = f"Meaning of __{input_str}__\n"
+    word = event.pattern_match.group(1)
+    urban = asyncurban.UrbanDictionary()
     try:
-        response = requests.get(input_url, headers=headers).json()
-        pronounciation = response.get("p")
-        meaning_dict = response.get("lwo")
-        for current_meaning in meaning_dict:
-            current_meaning_type = current_meaning.get("type")
-            current_meaning_definition = current_meaning.get("definition")
-            caption_str += f"**{current_meaning_type}**: {current_meaning_definition}\n\n"
-    except Exception as e:
-        caption_str = str(e)
-    reply_msg_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_msg_id = event.reply_to_msg_id
-    try:
-        await borg.send_file(
-            event.chat_id,
-            pronounciation,
-            caption=f"Pronounciation of __{input_str}__",
-            force_document=False,
-            reply_to=reply_msg_id,
-            allow_cache=True,
-            voice_note=True,
-            silent=True,
-            supports_streaming=True
+        mean = await urban.get_word(word)
+        await edit_or_reply(
+            event,
+            "Text: **{}**\n\nMeaning: **{}**\n\nExample: __{}__".format(
+                mean.word, mean.definition, mean.example
+            ),
         )
-    except:
-        pass
-    await event.edit(caption_str)
+    except asyncurban.WordNotFoundError:
+        await edit_or_reply(event, "No result found for **" + word + "**")
+
+
+@bot.on(admin_cmd(pattern="meaning (.*)"))
+@bot.on(sudo_cmd(pattern="meaning (.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    word = event.pattern_match.group(1)
+    dictionary = PyDictionary()
+    hell = dictionary.meaning(word)
+    output = f"**Word :** __{word}__\n\n"
+    try:
+        for a, b in hell.items():
+            output += f"**{a}**\n"
+            for i in b:
+                output += f"☞__{i}__\n"
+        await edit_or_reply(event, output)
+    except Exception:
+        await edit_or_reply(event, f"Couldn't fetch meaning of {word}")
+
+
+CMD_HELP.update(
+    {
+        "dictionary": "**Plugin :** `dictionary`\
+    \n\n**Syntax :** `.ud query`\
+    \n**Usage : **fetches meaning from Urban dictionary\
+    \n\n**Syntax : **`.meaning query`\
+    \n**Usage : **Fetches meaning of the given word\
+    "
+    }
+)
