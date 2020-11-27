7bbee79711c69045bc0f14c81b47f8cfe0c2e998
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-28 01:54:02 +0530

    Update invite.py

--- userbot/plugins/invite.py
+++ userbot/plugins/invite.py
@@ -101,43 +101,6 @@ async def get_users(event):
     return await hell.edit(f"**Terminal Finished** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people")
 
 
-@bot.on(admin_cmd(pattern="add ?(.*)"))
-@bot.on(sudo_cmd(pattern="add ?(.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    to_add_users = event.pattern_match.group(1)
-    if event.is_private:
-        await edit_delete(
-            event, "`.add` users to a chat, not to a Private Message", 5
-        )
-    else:
-        if not event.is_channel and event.is_group:
-            # https://lonamiwebs.github.io/Telethon/methods/messages/add_chat_user.html
-            for user_id in to_add_users.split(" "):
-                try:
-                    await event.client(
-                        functions.messages.AddChatUserRequest(
-                            chat_id=event.chat_id, user_id=user_id, fwd_limit=1000000
-                        )
-                    )
-                except Exception as e:
-                    await edit_delete(event, f"`{str(e)}`", 5)
-        else:
-            # https://lonamiwebs.github.io/Telethon/methods/channels/invite_to_channel.html
-            for user_id in to_add_users.split(" "):
-                try:
-                    await event.client(
-                        functions.channels.InviteToChannelRequest(
-                            channel=event.chat_id, users=[user_id]
-                        )
-                    )
-                except Exception as e:
-                    await edit_delete(event, f"`{str(e)}`", 5)
-
-        await edit_or_reply(event, f"`Added {to_add_users} Successfully😄`")
-
-
 CMD_HELP.update(
     {
         "invite": """**Plugin : **`invite`
