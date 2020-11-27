5cdd086b9a6884c98b6e46c633aa4d5db0d35947
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-28 00:58:24 +0530

    Update links.py

--- userbot/plugins/links.py
+++ userbot/plugins/links.py
@@ -9,8 +9,8 @@ from telethon import events
 import os
 import requests
 import json
-from userbot.utils import admin_cmd, CMD_HELP
-
+from userbot.utils import admin_cmd
+from userbot import CMD_HELP
 
 @borg.on(admin_cmd("dns (.*)"))
 async def _(event):
