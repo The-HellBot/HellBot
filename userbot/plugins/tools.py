18c94a6115aa4d0fc07d1e60a530babf6b566dc5
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-27 23:13:16 +0530

    Update tools.py

--- userbot/plugins/tools.py
+++ userbot/plugins/tools.py
@@ -187,24 +187,40 @@ async def make_qr(makeqr):
     await makeqr.delete()
 
 
+from telethon import events
+import asyncio
+from datetime import datetime
+import requests
+import json
+from uniborg.util import admin_cmd
+
+
 @bot.on(admin_cmd(pattern="cal (.*)"))
 @bot.on(sudo_cmd(pattern="cal (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
+    start = datetime.now()
     input_str = event.pattern_match.group(1)
-    input_sgra = input_str.split(";")
-    if len(input_sgra) == 2:
+    input_sgra = input_str.split(".")
+    if len(input_sgra) == 3:
         yyyy = input_sgra[0]
         mm = input_sgra[1]
-        try:
-            input = calendar.month(int(yyyy.strip()), int(mm.strip()))
-            await edit_or_reply(event, f"```{input}```")
-        except Exception as e:
-            await edit_delete(event, f"`{e}`", 5)
+        dd = input_sgra[2]
+        required_url = "https://calendar.kollavarsham.org/api/years/{}/months/{}/days/{}?lang={}".format(yyyy, mm, dd, "en")
+        headers = {"Accept": "application/json"}
+        response_content = requests.get(required_url, headers=headers).json()
+        a = ""
+        if "error" not in response_content:
+            current_date_detail_arraays = response_content["months"][0]["days"][0]
+            a = json.dumps(current_date_detail_arraays, sort_keys=True, indent=4)
+        else:
+            a = response_content["error"]
+        await event.edit(str(a))
     else:
-        await edit_delete(event, "**Syntax : **`.cal year ; month `", 5)
-
+        await event.edit("SYNTAX: .calendar YYYY.MM.DD")
+    end = datetime.now()
+    ms = (end - start).seconds
 
 @bot.on(admin_cmd(pattern="currency (.*)"))
 @bot.on(sudo_cmd(pattern="currency (.*)", allow_sudo=True))
