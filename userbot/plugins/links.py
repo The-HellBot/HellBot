52e04f09f8b7299e79b673f43fdbb9bad806e7c4
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-11-27 22:56:45 +0530

    Update links.py

--- userbot/plugins/links.py
+++ userbot/plugins/links.py
@@ -1,127 +1,54 @@
+""DA.GD helpers in @UniBorg
+Available Commands:
+.isup URL
+.dns google.com
+.url <long url>
+.unshort <short url>"""
+from telethon import events
+import os
 import requests
-from validators.url import url
-
-from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot import CMD_HELP
+import json
+from userbot.utils import admin_cmd
 
 
-@bot.on(admin_cmd(pattern="dns( (.*)|$)"))
-@bot.on(sudo_cmd(pattern="dns( (.*)|$)", allow_sudo=True))
+@borg.on(admin_cmd("dns (.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    input_str = "".join(event.text.split(maxsplit=1)[1:])
-    reply = await event.get_reply_message()
-    if not input_str and reply:
-        input_str = reply.text
-    if not input_str:
-        return await edit_delete(
-            event, "`Either reply to link or give link as input to get data`", 5
-        )
-    check = url(input_str)
-    if not check:
-        hellbotstr = "http://" + input_str
-        check = url(hellbotstr)
-    if not check:
-        return await edit_delete(event, "`the given link is not supported`", 5)
-    sample_url = f"https://da.gd/dns/{input_str}"
+    input_str = event.pattern_match.group(1)
+    sample_url = "https://da.gd/dns/{}".format(input_str)
     response_api = requests.get(sample_url).text
     if response_api:
-        await edit_or_reply(event, f"DNS records of {input_str} are \n{response_api}")
+        await event.edit("DNS records of {} are \n{}".format(input_str, response_api))
     else:
-        await edit_or_reply(
-            event, f"__I can't seem to find `{input_str}` on the internet__"
-        )
+        await event.edit("i can't seem to find {} on the internet".format(input_str))
 
 
-@bot.on(admin_cmd(pattern="url( (.*)|$)"))
-@bot.on(sudo_cmd(pattern="url( (.*)|$)", allow_sudo=True))
+@borg.on(admin_cmd("url (.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    input_str = "".join(event.text.split(maxsplit=1)[1:])
-    reply = await event.get_reply_message()
-    if not input_str and reply:
-        input_str = reply.text
-    if not input_str:
-        return await edit_delete(
-            event, "`Either reply to link or give link as input to get data`", 5
-        )
-    check = url(input_str)
-    if not check:
-        hellbotstr = f"http://" + input_str
-        check = url(hellbotstr)
-    if not check:
-        return await edit_delete(event, "`the given link is not supported`", 5)
-    if not input_str.startswith("http"):
-        input_str = "http://" + input_str
-    sample_url = f"https://da.gd/s?url={input_str}"
+    input_str = event.pattern_match.group(1)
+    sample_url = "https://da.gd/s?url={}".format(input_str)
     response_api = requests.get(sample_url).text
     if response_api:
-        await edit_or_reply(
-            event, f"Generated {response_api} for {input_str}.", link_preview=False
-        )
+        await event.edit("Generated {} for {}.".format(response_api, input_str))
     else:
-        await edit_or_reply(event, "`Something is wrong, please try again later.`")
+        await event.edit("something is wrong. please try again later.")
 
 
-@bot.on(admin_cmd(pattern="unshort( (.*)|$)"))
-@bot.on(sudo_cmd(pattern="unshort( (.*)|$)", allow_sudo=True))
+@borg.on(admin_cmd("unshort (.*)"))
 async def _(event):
     if event.fwd_from:
         return
-    input_str = "".join(event.text.split(maxsplit=1)[1:])
-    reply = await event.get_reply_message()
-    if not input_str and reply:
-        input_str = reply.text
-    if not input_str:
-        return await edit_delete(
-            event, "`Either reply to link or give link as input to get data`", 5
-        )
-    check = url(input_str)
-    if not check:
-        hellbotstr = "http://" + input_str
-        check = url(hellbotstr)
-    if not check:
-        return await edit_delete(event, "`the given link is not supported`", 5)
+    input_str = event.pattern_match.group(1)
     if not input_str.startswith("http"):
         input_str = "http://" + input_str
     r = requests.get(input_str, allow_redirects=False)
-    if str(r.status_code).startswith("3"):
-        await edit_or_reply(
-            event,
-            f"Input URL: {input_str}\nReDirected URL: {r.headers['Location']}",
-            link_preview=False,
-        )
+    if str(r.status_code).startswith('3'):
+        await event.edit("Input URL: {}\nReDirected URL: {}".format(input_str, r.headers["Location"]))
     else:
-        await edit_or_reply(
-            event,
-            "Input URL {} returned status_code {}".format(input_str, r.status_code),
-        )
-
-
-# By Priyam Kalra
-@bot.on(admin_cmd(pattern="hl( (.*)|$)"))
-@bot.on(sudo_cmd(pattern="hl( (.*)|$)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = "".join(event.text.split(maxsplit=1)[1:])
-    reply = await event.get_reply_message()
-    if not input_str and reply:
-        input_str = reply.text
-    if not input_str:
-        return await edit_delete(
-            event, "`Either reply to link or give link as input to get data`", 5
-        )
-    check = url(input_str)
-    if not check:
-        hellbotstr = "http://" + input_str
-        check = url(hellbotstr)
-    if not check:
-        return await edit_delete(event, "`the given link is not supported`", 5)
-    await edit_or_reply(event, "[ㅤㅤㅤㅤㅤㅤㅤ](" + input_str + ")")
-
+        await event.edit("Input URL {} returned status_code {}".format(input_str, r.status_code))
 
 CMD_HELP.update(
     {
