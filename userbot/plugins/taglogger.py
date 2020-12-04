d56099b9a834d58573109ce09ce69356160a329f
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-04 11:36:23 +0530

    HellBot V1.9🔥
    ​​​​»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»
    🔥 New Update Available 🔥
    
    
    🔸🔹Hellbot v1.9 available now
            
    🔹🔸 ADDED SUDO IN MORE PLUGINS
    
            ★ Added sudo in almost all modules...
            ★ I'll add full sudo support till 2.0
    
    🔸🔹NEW MODULE
    
            ★ Added a new module to protect your group from spammers. (Like me)
    
    🔹🔸 CUSTOM ALIVE IMAGE
            
            ★ You can now add your own custom alive pic & msg. #Improved
            ★ Type .set var ALIVE_PIC <your link> without bracket
            ★ Note that the cmd is .hell . Just improved it...🙃🙃
            
    🔸🔹FIXED TRANSLATE
    
    🔹🔸FIXED STICKERS
          
             ★ Fixed kanging animated stickers.
             ★ I saw some users weren't able to kang stickers because they weren't having username. So i added "CUSTOM STICKER PACK NAME".
             ★ To set your custom sticker pack name, Type .set var CUSTOM_STICKER_PACK_NAME <your pack name> without bracket
    
    🔸🔹ADDED PRANKS 
     
             ★ Added some more image memes...
             ★ Type .plinfo pranks or .help pranks for more infos...
    
    🔹🔸SOME IMPROVEMENTS
    
    🔸🔹MINOR BUG FIXES
    
            To update your Hêllẞø†
       • Type .update
    ≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠
     Haters Gand marao apni... IDC🤙
    ≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠
    Stay Tuned............
    
    @HellBot_Official
    »»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»

---
+++ userbot/plugins/taglogger.py
@@ -0,0 +1,58 @@
+from telethon import custom, events, Button
+from telethon.tl.types import Channel
+from telethon.utils import get_display_name
+
+from userbot.uniborgConfig import Config
+
+if Config.PM_LOGGR_BOT_API_ID:
+    tagger = int(Config.PM_LOGGR_BOT_API_ID)
+
+if Config.PM_LOGGR_BOT_API_ID:
+
+    @bot.on(
+        events.NewMessage(
+            incoming=True,
+            blacklist_chats=Config.UB_BLACK_LIST_CHAT,
+            func=lambda e: (e.mentioned),
+        )
+    )
+    async def all_messages_catcher(event):
+        # the bot might not have the required access_hash to mention the
+        # appropriate PM
+        await event.forward_to(Var.TG_BOT_USER_NAME_BF_HER)
+
+        # construct message
+        # the message format is stolen from @MasterTagAlertBot
+        ammoca_message = ""
+
+        kraken = await event.client.get_entity(event.sender_id)
+        if kraken.bot or kraken.verified or kraken.support:
+            return
+
+        krakenm = f"[{get_display_name(kraken)}](tg://user?id={kraken.id})"
+
+        where_ = await event.client.get_entity(event.chat_id)
+
+        where_m = get_display_name(where_)
+        button_text = "See the tag 📬"
+
+        if isinstance(where_, Channel):
+            message_link = f"https://t.me/c/{where_.id}/{event.id}"
+        else:
+            # not an official link,
+            # only works in DrKLO/Telegram,
+            # for some reason
+            message_link = f"tg://openmessage?chat_id={where_.id}&message_id={event.id}"
+            # Telegram is weird :\
+
+        ammoca_message += f"{krakenm} `just tagged you...` \nWhere?\nIn [{where_m}]({message_link})\n__Tap to go the tagged msg__📬🚶"
+        if tagger is not None:
+            await bot.send_message(
+                entity=tagger,
+                message=ammoca_message,
+                link_preview=False,
+                buttons=[[custom.Button.url(button_text, message_link)]],
+                silent=True,
+            )
+        else:
+            return
