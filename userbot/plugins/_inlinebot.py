0cdf25c32eca21465a031ecad7dc16fe69c2b3c4
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-04 20:01:35 +0530

    Perfect

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -17,7 +17,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                "@HellBot_Official",
+                "© Userbot Help",
                 text="{}\nCurrently Loaded Plugins: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use Mine\n Check @HellBot_Official For The Source!"
+            reply_pop_up_alert = "Kya dabae ja rha h bsdk. Apna khudka bot bna jake... @HellBot_Official!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -55,8 +55,16 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use Mine\n Check @HellBot_Official For The Source!"
+            reply_pop_up_alert = "Kya dabae ja rha h bsdk. Apna khudka bot bna jake... @HellBot_Official!!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
+    async def on_plug_in_callback_query_handler(event):
+        if event.query.user_id == bot.uid:
+            await event.edit("⚜️Hêllẞø† Menu Provider Is now Closed⚜️\n\n      © Hêllẞø† ™")
+
+
+
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
     ))
@@ -74,20 +82,12 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             reply_pop_up_alert = help_string
         reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
-            © @HellBot_Official".format(plugin_name)
+            © Hêllẞø†™".format(plugin_name)
         try:
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-        except:
-            with io.BytesIO(str.encode(reply_pop_up_alert)) as out_file:
-                out_file.name = "{}.txt".format(plugin_name)
-                await event.client.send_file(
-                    event.chat_id,
-                    out_file,
-                    force_document=True,
-                    allow_cache=False,
-                    caption="Uploaded By @HellBot_Official"
-                )
-
+        except: 
+            halps = "Do .help {} to get the list of commands.".format(plugin_name)
+            await event.answer(halps, cache_time=0, alert=True)
 
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD
@@ -99,25 +99,19 @@ def paginate_help(page_number, loaded_plugins, prefix):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {} {}".format(random.choice(list(multi)), x, random.choice(list(multi))),
+        "{} {}".format(random.choice(list(multi)), x, random.choice(list(multi))),
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
-    if number_of_cols == 1:
-        pairs = list(zip(modules[::number_of_cols]))
-    elif number_of_cols == 2:
-        pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
-    else:
-        pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols], modules[2::number_of_cols]))
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
-    elif len(modules) % number_of_cols == 2:
-        pairs.append((modules[-2],modules[-1]))
     max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("Previous", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("Next", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("• 👈 •", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("• ❌ •", data="close"),
+             custom.Button.inline("• 👉 •", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
