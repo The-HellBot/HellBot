e5581bef2912bb1081aa4c20f672a32eff8fe828
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-07 14:55:28 +0530

    Create hellbot.sh

---
+++ hellbot.sh
@@ -0,0 +1,71 @@
+set -euo pipefail
+
+
+echo '
+•Deployment process started•
+'
+export DEBIAN_FRONTEND=noninteractive
+export TZ=Asia/Kolkata
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+apt-get update && apt upgrade -y 
+apt-get install -y --no-install-recommends \
+    coreutils \
+    bash \
+    nodejs \
+    bzip2 \
+    curl \
+    figlet \
+    gcc \
+    g++ \
+    git \
+    util-linux \
+    libevent-dev \
+    libjpeg-dev \
+    libffi-dev \
+    libpq-dev \
+    libwebp-dev \
+    libxml2 \
+    libxml2-dev \
+    libxslt-dev \
+    musl \
+    neofetch \
+    libcurl4-openssl-dev \
+    postgresql \
+    postgresql-client \
+    postgresql-server-dev-all \
+    openssl \
+    mediainfo \
+    wget \
+    python3 \
+    python3-dev \
+    python3-pip \
+    libreadline-dev \
+    zipalign \
+    sqlite \
+    ffmpeg \
+    libsqlite3-dev \
+    zlib1g-dev \
+    recoverjpeg \
+    zip \
+    megatools \
+    libfreetype6-dev \
+    procps \
+    policykit-1\
+    unzip
+apt autoremove --yes
+
+pip3 install --upgrade pip setuptools 
+git clone https://github.com/HellBoy-OP/HellBot ./
+mkdir bin && mkdir pikabot
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt install -y ./google-chrome-stable_current_amd64.deb
+rm -f ./google-chrome-stable_current_amd64.deb
+wget https://chromedriver.storage.googleapis.com/86.0.4240.22/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod +x chromedriver && mv -f chromedriver /usr/bin/ && rm -f ./chromedriver_linux64
+pip3 install -r requirements.txt 
+
+echo '•Deployment Finished•
+Thank You For Deploying HellBot
+• Wait While HellBot is being pushed to Heroku
+• Turn your Dynos on
+If You face any problem then contact admins at @HellBot_Official_Chat
+'
