5ba459ce7c1ad481132cb70ed7964971e27f5175
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-09 19:45:04 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,14 +1,17 @@
-from userbot import bot
-from sys import argv
+from userbot import *
+from sys import *
 import sys
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 import os
-from telethon import TelegramClient
+from telethon import TelegramClient, functions, types 
+from telethon.tl.types import InputMessagesFilterDocument 
 from var import Var
 from userbot.utils import load_module
 from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
 from pathlib import Path
 import asyncio
+import importlib
+import traceback
 import telethon.utils
 
 async def add_bot(bot_token):
@@ -47,13 +50,29 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
-import userbot._core
-
-print("Aree On Ho Gaya Bhenchod!! Abb jake .alive likh ke dekho!! Abb Jaao aur sabki maroo.... Join @HellBot_Official for any help..")
-
-if len(argv) not in (1, 3, 4):
-    bot.disconnect()
-else:
-    bot.run_until_disconnected()
 
 
+plugin_channel = "@module_hellbot"
+
+test1 = await client.get_messages(plugin_channel, None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
+  for ixo in total_doxx:
+       mxo = test1[ixo].id ; await client.download_media(await client.get_messages(cIient, ids=mxo), "userbot/plugins/")
+  ar = glob.glob("userbot/plugins/*.py")
+  f = len(ar)
+  LOGS.info(f" loading {f} modules it may take 1 minute please wait")
+  for i in ar:
+     br = os.path.basename(i)
+     cr = (os.path.splitext(br)[0])
+     import_module(f"userbot.plugins.{cr}")
+     la += 1
+     LOGS.info(f" loaded {la}/{f} modules")  
+  os.system("rm userbot/plugins/*.py") ; LOGS.info(f"Sucessfully deployed your Hêllẞø†. Type .ping or .alive check if its on. For help join @HellBot_Official")
+  if len(argv) not in (1, 3, 4):
+       await bot.disconnect()
+  else:
+       await bot.run_until_disconnected()
+       
+
+        
+
+bot.loop.run_until_complete(a())
