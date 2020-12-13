966e57eaec2b5e48dd4ded40db1831326270acc9
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-13 14:44:40 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -54,6 +54,92 @@ for i in ALL_ROWS:
     BRAIN_CHECKER.append(i[0])
 connect("learning-data-root.check").close()
 
+
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+# Asena UserBot - Yusuf Usta
+
+""" UserBot başlangıç noktası """
+import importlib
+from importlib import import_module
+from sqlite3 import connect
+import os
+import requests
+from telethon.tl.types import InputMessagesFilterDocument
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+from telethon.tl.functions.channels import GetMessagesRequest
+from . import BRAIN_CHECKER, LOGS, bot, PLUGIN_CHANNEL_ID, CMD_HELP, LANGUAGE, ASENA_VERSION, PATTERNS
+from .modules import ALL_MODULES
+import userbot.modules.sql_helper.mesaj_sql as MSJ_SQL
+import userbot.modules.sql_helper.galeri_sql as GALERI_SQL
+from pySmartDL import SmartDL
+from telethon.tl import functions
+
+from random import choice
+import chromedriver_autoinstaller
+from json import loads, JSONDecodeError
+import re
+import userbot.cmdhelp
+
+DIZCILIK_STR = [
+    "Çıkartmayı dızlıyorum...",
+    "Yaşasın dızcılık...",
+    "Bu çıkartmayı kendi paketime davet ediyorum...",
+    "Bunu dızlamam lazım...",
+    "Hey bu güzel bir çıkartma!\nHemen dızlıyorum..",
+    "Çıkartmanı dızlıyorum\nhahaha.",
+    "Hey şuraya bak. (☉｡☉)!→\nBen bunu dızlarken...",
+    "Güller kırmızı menekşeler mavi, bu çıkartmayı paketime dızlayarak havalı olacağım...",
+    "Çıkartma hapsediliyor...",
+    "Bay dızcı bu çıkartmayı dızlıyor... ",
+]
+
+AFKSTR = [
+    "Şu an acele işim var, daha sonra mesaj atsan olmaz mı? Zaten yine geleceğim.",
+    "Aradığınız kişi şu anda telefona cevap veremiyor. Sinyal sesinden sonra kendi tarifeniz üzerinden mesajınızı bırakabilirsiniz. Mesaj ücreti 49 kuruştur. \n`biiiiiiiiiiiiiiiiiiiiiiiiiiiiip`!",
+    "Birkaç dakika içinde geleceğim. Fakat gelmezsem...\ndaha fazla bekle.",
+    "Şu an burada değilim, muhtemelen başka bir yerdeyim.",
+    "Güller kırmızı\nMenekşeler mavi\nBana bir mesaj bırak\nVe sana döneceğim.",
+    "Bazen hayattaki en iyi şeyler beklemeye değer…\nHemen dönerim.",
+    "Hemen dönerim,\nama eğer geri dönmezsem,\ndaha sonra dönerim.",
+    "Henüz anlamadıysan,\nburada değilim.",
+    "Merhaba, uzak mesajıma hoş geldiniz, bugün sizi nasıl görmezden gelebilirim?",
+    "7 deniz ve 7 ülkeden uzaktayım,\n7 su ve 7 kıta,\n7 dağ ve 7 tepe,\n7 ovala ve 7 höyük,\n7 havuz ve 7 göl,\n7 bahar ve 7 çayır,\n7 şehir ve 7 mahalle,\n7 blok ve 7 ev...\n\nMesajların bile bana ulaşamayacağı bir yer!",
+    "Şu anda klavyeden uzaktayım, ama ekranınızda yeterince yüksek sesle çığlık atarsanız, sizi duyabilirim.",
+    "Şu yönde ilerliyorum\n---->",
+    "Şu yönde ilerliyorum\n<----",
+    "Lütfen mesaj bırakın ve beni zaten olduğumdan daha önemli hissettirin.",
+    "Sahibim burada değil, bu yüzden bana yazmayı bırak.",
+    "Burada olsaydım,\nSana nerede olduğumu söylerdim.\n\nAma ben değilim,\ngeri döndüğümde bana sor...",
+    "Uzaklardayım!\nNe zaman dönerim bilmiyorum !\nUmarım birkaç dakika sonra!",
+    "Sahibim şuan da müsait değil. Adınızı, numarınızı ve adresinizi verirseniz ona iletibilirm ve böylelikle geri döndüğü zaman.",
+    "Üzgünüm, sahibim burada değil.\nO gelene kadar benimle konuşabilirsiniz.\nSahibim size sonra döner.",
+    "Bahse girerim bir mesaj bekliyordun!",
+    "Hayat çok kısa, yapacak çok şey var...\nOnlardan birini yapıyorum...",
+    "Şu an burada değilim....\nama öyleysem ...\n\nbu harika olmaz mıydı?",
+]
+
+UNAPPROVED_MSG = ("`Hey,` {mention}`! Bu bir bot. Endişelenme.\n\n`"
+                  "`Sahibim sana PM atma izni vermedi. `"
+                  "`Lütfen sahibimin aktif olmasını bekleyin, o genellikle PM'leri onaylar.\n\n`"
+                  "`Bildiğim kadarıyla o kafayı yemiş insanlara PM izni vermiyor.`")
+
+DB = connect("learning-data-root.check")
+CURSOR = DB.cursor()
+CURSOR.execute("""SELECT * FROM BRAIN1""")
+ALL_ROWS = CURSOR.fetchall()
+INVALID_PH = '\nHATA: Girilen telefon numarası geçersiz' \
+             '\n  Ipucu: Ülke kodunu kullanarak numaranı gir' \
+             '\n       Telefon numaranızı tekrar kontrol edin'
+
+for i in ALL_ROWS:
+    BRAIN_CHECKER.append(i[0])
+connect("learning-data-root.check").close()
+
 def extractCommands(file):
     FileRead = open(file, 'r').read()
     
@@ -61,33 +147,34 @@ def extractCommands(file):
         file = file.split('/')[-1]
 
     Pattern = re.findall(r"@register\(.*pattern=(r|)\"(.*)\".*\)", FileRead)
-    Commands = []
+    Komutlar = []
 
     if re.search(r'CmdHelp\(.*\)', FileRead):
         pass
     else:
-      dosyaAdi = file.replace('.py', '')
-      CmdHelp = userbot.cmdhelp.CmdHelp(dosyaAdi, False)
+        dosyaAdi = file.replace('.py', '')
+        CmdHelp = userbot.cmdhelp.CmdHelp(dosyaAdi, False)
+
         for Command in Pattern:
             Command = Command[1]
             if Command == '' or len(Command) <= 1:
                 continue
-            Command = re.findall("(^.*[a-zA-Z0-9şğüöçı]\w)", Command)
-            if (len(Command) >= 1) and (not Command[0] == ''):
-                Command = Command[0]
-                if Command[0] == '^':
-                    CommandStr = Command[1:]
-                    if CommandStr[0] == '.':
-                        CommandStr = CommandStr[1:]
-                    Commands.append(CommandStr)
+            Komut = re.findall("(^.*[a-zA-Z0-9şğüöçı]\w)", Command)
+            if (len(Komut) >= 1) and (not Komut[0] == ''):
+                Komut = Komut[0]
+                if Komut[0] == '^':
+                    KomutStr = Komut[1:]
+                    if KomutStr[0] == '.':
+                        KomutStr = KomutStr[1:]
+                    Komutlar.append(KomutStr)
                 else:
                     if Command[0] == '^':
-                        CommandStr = Command[1:]
-                        if CommandStr[0] == '.':
-                            CommandStr = CommandStr[1:]
+                        KomutStr = Command[1:]
+                        if KomutStr[0] == '.':
+                            KomutStr = KomutStr[1:]
                         else:
-                            CommandStr = Command
-                        Commands.append(CommandStr)
+                            KomutStr = Command
+                        Komutlar.append(KomutStr)
 
             Hellbot = re.search('\"\"\"HELLBOT(.*)\"\"\"', FileRead, re.DOTALL)
             if not Hellbot == None:
