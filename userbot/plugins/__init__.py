475fcd96025c036fd82b52c1375df429628d77b0
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-13 12:33:42 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,6 +1,18 @@
-from userbot import simpdef
+from userbot import LOGS
 
 
-simpmusic = simpdef.simpmusic 
-simpmusicvideo = simpdef.simpmusicvideo
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
 
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3] for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+LOGS.info("modules to be installed: %s", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
