f2a58b16a4321c27ccaf06a0918a4ef552da6de2
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-13 18:12:46 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -20,6 +20,15 @@ else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 
+if not Var.TG_BOT_TOKEN_BF_HER == None:
+    tgbot = TelegramClient(
+        "TG_BOT_TOKEN_BF_HER",
+        api_id=Var.API_ID,
+        api_hash=Var.API_HASH
+    ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+else:
+    tgbot = None
+
 
 CMD_LIST = {}
 # for later purposes
