db3c5ec8d35ef3e52a728a4511a5ba37d84f9911
HellBoy-OP <ghosthellboy4@gmail.com>
2020-12-13 13:07:49 +0000

    pylint: auto fixes

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -22,9 +22,7 @@ else:
 
 if not Var.TG_BOT_TOKEN_BF_HER == None:
     tgbot = TelegramClient(
-        "TG_BOT_TOKEN",
-        api_id=Var.APP_ID,
-        api_hash=Var.API_HASH
+        "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
     ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
 else:
     tgbot = None
