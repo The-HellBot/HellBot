4734f10340ffb0b3dd128e2a4ddc2e574229e9ae
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-16 01:06:00 +0530

    Update animations2.py

--- userbot/plugins/animations2.py
+++ userbot/plugins/animations2.py
@@ -3,6 +3,7 @@ from collections import deque
 
 from userbot import *
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot.cmdhelp import CmdHelp
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
 
