8e1b050d972ae5d8cd2f09590415698e9c177ba5
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-17 23:15:29 +0530

    update fileext.py

--- userbot/plugins/fileext.py
+++ userbot/plugins/fileext.py
@@ -1,15 +1,15 @@
-"""Get info about a File Extension
-Syntax: .filext EXTENSION"""
 import requests
 from bs4 import BeautifulSoup
-from uniborg.util import admin_cmd
 
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot import CMD_HELP
+from userbot.cmdhelp import CmdHelp
 
-@borg.on(admin_cmd(pattern="filext (.*)"))
+@bot.on(admin_cmd(pattern="filext (.*)"))
+@bot.on(sudo_cmd(pattern="filext (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("Processing ...")
     sample_url = "https://www.fileext.com/file-extension/{}.html"
     input_str = event.pattern_match.group(1).lower()
     response_api = requests.get(sample_url.format(input_str))
@@ -18,14 +18,21 @@ async def _(event):
         raw_html = response_api.content
         soup = BeautifulSoup(raw_html, "html.parser")
         ext_details = soup.find_all("td", {"colspan": "3"})[-1].text
-        await event.edit(
+        await edit_or_reply(
+            event,
             "**File Extension**: `{}`\n**Description**: `{}`".format(
                 input_str, ext_details
-            )
+            ),
         )
     else:
-        await event.edit(
+        await edit_or_reply(
+            event,
             "https://www.fileext.com/ responded with {} for query: {}".format(
                 status_code, input_str
-            )
+            ),
         )
+
+
+CmdHelp("fileext").add_command(
+  'filext', '<extension name>', 'Shows you the detailed information of that extension type.'
+).add()
