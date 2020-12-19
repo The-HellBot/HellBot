faaf05598b6327401ed80ae4b6594915ee3fbf0b
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-20 00:12:12 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,18 +1,21 @@
-from pathlib import Path
-from sys import argv
-
-import telethon.utils
-from telethon import TelegramClient
-
 from userbot import bot
-from userbot.utils import load_module
+from sys import argv
+import sys
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+import os
+from telethon import TelegramClient
 from var import Var
-
+from userbot.utils import load_module
+from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
+from pathlib import Path
+import asyncio
+import telethon.utils
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
-    bot.me = await bot.get_me()
+    bot.me = await bot.get_me() 
     bot.uid = telethon.utils.get_peer_id(bot.me)
+
 
 
 if len(argv) not in (1, 3, 4):
@@ -23,7 +26,9 @@ else:
         print("Initiating Inline Bot")
         # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
-            "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished with no errors")
         print("Starting Userbot")
@@ -34,8 +39,7 @@ else:
 
 
 import glob
-
-path = "userbot/plugins/*.py"
+path = 'userbot/plugins/*.py'
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
@@ -43,10 +47,9 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
+import userbot._core
 
-print(
-    "Aree On Ho Gaya Bhenchod!! Abb jake .alive likh ke dekho!! Abb Jaao aur sabki maroo.... Join @HellBot_Official for any help.."
-)
+print("Aree On Ho Gaya Bhenchod!! Abb jake .alive likh ke dekho!! Abb Jaao aur sabki maroo.... Join @HellBot_Official for any help..")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
