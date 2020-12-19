33d3c4832fdc8ea580e6a387b725af2f7d7abeb2
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-19 12:10:08 +0530

    update animations2.py

--- userbot/plugins/animations2.py
+++ userbot/plugins/animations2.py
@@ -227,7 +227,35 @@ async def _(event):
     for i in animation_ttl:
         await asyncio.sleep(animation_interval)
         await event.edit(animation_chars[i % 12])
-
+        
+@bot.on(admin_cmd(pattern=r"hack$", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"hack$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 2
+    animation_ttl = range(15)
+    event = await edit_or_reply(event, "`Hacking targeted Account.`")
+animation_chars = [
+            "Looking for WhatsApp databases in targeted person...",
+            " User online: True\nTelegram access: True\nRead Storage: True ",
+            "Hacking... 0%\n[░░░░░░░░░░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 20s",
+            "Hacking... 11.07%\n[██░░░░░░░░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 18s",
+            "Hacking... 20.63%\n[███░░░░░░░░░░░░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 16s",
+            "Hacking... 34.42%\n[█████░░░░░░░░░░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 14s",
+            "Hacking... 42.17%\n[███████░░░░░░░░░░░░░]\n`Searching for databases`\nETA: 0m, 12s",
+            "Hacking... 55.30%\n[█████████░░░░░░░░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 10s",
+            "Hacking... 64.86%\n[███████████░░░░░░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 08s",
+            "Hacking... 74.02%\n[█████████████░░░░░░░]\n`Trying to Decrypt...`\nETA: 0m, 06s",
+            "Hacking... 86.21%\n[███████████████░░░░░]\n`Trying to Decrypt...`\nETA: 0m, 04s",
+            "Hacking... 93.50%\n[█████████████████░░░]\n`Decryption successful!`\nETA: 0m, 02s",
+            "Hacking... 100%\n[████████████████████]\n`Scanning file...`\nETA: 0m, 00s",
+            "Hacking complete!\nUploading file...",
+            "Targeted Account Hacked...!\n\n ✅ File has been successfully uploaded to my server.\nWhatsApp Database:\n`./DOWNLOADS/msgstore.db.crypt12`",
+        ]
+        for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 15])
 
 @bot.on(admin_cmd(pattern=r"jio$", outgoing=True))
 @bot.on(sudo_cmd(pattern=r"jio$", allow_sudo=True))
@@ -347,4 +375,6 @@ CmdHelp("animations2").add_command(
   'degi', None, 'Use and see'
 ).add_command(
   'nehi', None, 'Use and see'
+).add_command(
+  'hack', None, 'Im a hacker bitch'
 ).add()
