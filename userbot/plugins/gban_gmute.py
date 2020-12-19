a40b4972dc8b395e5640737a55bb95f69b03ce10
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-19 20:16:01 +0530

    Update gban_gmute.py

--- userbot/plugins/gban_gmute.py
+++ userbot/plugins/gban_gmute.py
@@ -38,6 +38,37 @@ UNBAN_RIGHTS = ChatBannedRights(
     embed_links=None,
 )
 
+async def get_user_from_event(event, secondgroup=None):
+    if secondgroup:
+        args = event.pattern_match.group(2).split(" ", 1)
+    else:
+        args = event.pattern_match.group(1).split(" ", 1)
+    extra = None
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_obj = await event.client.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif args:
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            await edit_or_reply(event, "`Pass the user's username, id or reply!`")
+            return None, None
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj, extra
+        try:
+            user_obj = await event.client.get_entity(user)
+        except (TypeError, ValueError):
+            return None, None
+    return user_obj, extra
+
 async def admin_groups(kraken):
     krakengroups = []
     async for dialog in kraken.client.iter_dialogs():
