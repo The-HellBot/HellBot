e90fa0ef605240ff87fb7c3d0f9cbdbace583a24
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-21 10:18:11 +0530

    Create all_plugins.py

---
+++ userbot/plugins/all_plugins.py
@@ -0,0 +1,10 @@
+
+from userbot import *
+from userbot.utils import *
+from userbot.cmdhelp import CmdHelp
+from userbot.uniborgConfig import Config
+from userbot.helpers.functions import *
+
+"""
+Teri maa ke bhosde me hai. Jake dekh le bhadwe..
+"""
