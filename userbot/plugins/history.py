290d4817a8f7e4a31a676990fa6bf004a1a74873
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-23 21:58:49 +0530

    update history.py

--- userbot/plugins/history.py
+++ userbot/plugins/history.py
@@ -6,26 +6,28 @@ logging.basicConfig(
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
 
 
-@bot.on(admin_cmd(pattern=("history ?(.*)")))
+@bot.on(admin_cmd(pattern="history ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="history ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     if not event.reply_to_msg_id:
-        await event.edit("```Reply to any user message.```")
+        await edit_or_reply(event, "```Reply to any user message.```")
         return
     reply_message = await event.get_reply_message()
     if not reply_message.text:
-        await event.edit("```reply to text message```")
+        await edit_or_reply(event, "```reply to text message```")
         return
     chat = "@SangMataInfo_bot"
     reply_message.sender
     if reply_message.sender.bot:
-        await event.edit("```Reply to actual users message.```")
+        await edit_or_reply(event, "```Reply to actual users message.```")
         return
-    await event.edit("```Processing```")
+    await edit_or_reply(event, "```Processing```")
     async with borg.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -34,11 +36,15 @@ async def _(event):
             await borg.forward_messages(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+            await edit_or_reply(event, "`Please unblock` @sangmatainfo_bot `and try again`")
             return
         if response.text.startswith("Forward"):
             await event.edit(
-                "```This user had disabled their forward privacy... Ask him to enable🥺```"
+                "```This user had disabled their forward privacy... Ask 'em to enable🥺```"
             )
         else:
-            await event.edit(f"{response.message.message}")
+            await edit_or_reply(event, f"{response.message.message}")
+
+CmdHelp("history").add_command(
+  "history", "<reply to a user>", "Fetches the Name history of the tagged user"
+).add()
