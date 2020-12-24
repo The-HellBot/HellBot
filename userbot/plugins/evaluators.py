2f922fd2a3cb33b0403a14a35cb5450f82dd3589
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-24 14:42:50 +0530

    Rename eval_exec.py to evaluators.py

---
+++ userbot/plugins/evaluators.py
@@ -0,0 +1,110 @@
+
+import asyncio
+import io
+import os
+import sys
+import traceback
+
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot import *
+from userbot.cmdhelp import CmdHelp
+
+@bot.on(admin_cmd(pattern="exec(?: |$|\n)(.*)", command="exec"))
+@bot.on(sudo_cmd(pattern="exec(?: |$|\n)(.*)", command="exec", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    cmd = "".join(event.text.split(maxsplit=1)[1:])
+    if not cmd:
+        return await edit_delete(event, "`What should i execute?..`")
+    hellevent = await edit_or_reply(event, "`Executing.....`")
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
+    helluser = await event.client.get_me()
+    if helluser.username:
+        curruser = helluser.username
+    else:
+        curruser = "hellbot"
+    uid = os.geteuid()
+    if uid == 0:
+        cresult = f"`{curruser}:~#` `{cmd}`\n`{result}`"
+    else:
+        cresult = f"`{curruser}:~$` `{cmd}`\n`{result}`"
+    await edit_or_reply(
+        event,
+        text=cresult,
+        aslink=True,
+        linktext=f"**•  Exec : **\n`{cmd}` \n\n**•  Result : **\n",
+    )
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            "Terminal command " + cmd + " was executed sucessfully.",
+        )
+
+
+@bot.on(admin_cmd(pattern="eval(?: |$|\n)(.*)", command="eval"))
+@bot.on(sudo_cmd(pattern="eval(?: |$|\n)(.*)", command="eval", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    cmd = "".join(event.text.split(maxsplit=1)[1:])
+    if not cmd:
+        return await edit_delete(event, "`What should i run ?..`")
+    hellevent = await edit_or_reply(event, "`Running ...`")
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        await aexec(cmd, event)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+    final_output = f"**•  Eval : **\n`{cmd}` \n\n**•  Result : **\n`{evaluation}` \n"
+    await edit_or_reply(
+        hellevent,
+        text=final_output,
+        aslink=True,
+        linktext=f"**•  Eval : **\n`{cmd}` \n\n**•  Result : **\n",
+    )
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            "eval command " + cmd + " was executed sucessfully.",
+        )
+
+
+async def aexec(code, smessatatus):
+    message = event = smessatatus
+    p = lambda _x: print(yaml_format(_x))
+    reply = await event.get_reply_message()
+    exec(
+        f"async def __aexec(message, event , reply, client, p, chat): "
+        + "".join(f"\n {l}" for l in code.split("\n"))
+    )
+    return await locals()["__aexec"](
+        message, event, reply, message.client, p, message.chat_id
+    )
+
+CmdHelp("eval_exec").add_command(
+  'eval', '<expr>', 'Execute python script'
+).add_command(
+  'exec', '<command>', 'Execute a Terminal command on HellBot server and shows details'
+).add()
