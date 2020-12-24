94d10a10f1ef57da7352e9c32a5392b136d81b8c
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-24 15:56:27 +0530

    update auto_profile.py

--- userbot/plugins/auto_profile.py
+++ userbot/plugins/auto_profile.py
@@ -3,9 +3,10 @@ import time
 
 from telethon.errors import FloodWaitError
 from telethon.tl import functions
+from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest
 
 from userbot import ALIVE_NAME, BIO_MSG, CMD_HELP
-from userbot.utils import admin_cmd, edit_or_reply
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
 DEFAULTUSERBIO = str(BIO_MSG) if BIO_MSG else "ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt"
@@ -87,8 +88,21 @@ async def _(event):
         await asyncio.sleep(DEL_TIME_OUT)
 
 
+@bot.on(admin_cmd(pattern="reserved", outgoing=True))
+@bot.on(sudo_cmd(pattern="reserved", allow_sudo=True))
+async def mine(event):
+    """ For .reserved command, get a list of your reserved usernames. """
+    result = await bot(GetAdminedPublicChannelsRequest())
+    output_str = ""
+    for channel_obj in result.chats:
+        output_str += f"{channel_obj.title}\n@{channel_obj.username}\n\n"
+    await edit_or_reply(event, output_str)
+
+
 CmdHelp("auto_profile").add_command(
   'autobio', None, 'Changes your bio with time. Need to set BIO_MSG in heroku vars(optional)'
 ).add_command(
   'autoname', None, 'Changes your name with time according to your ALIVE_NAME in heroku var'
+).add_command(
+  'reserved', None, 'Gives the list of usernames reserved by you. In short gives the list of public groups or channels that ouu are owner in.'
 ).add()
