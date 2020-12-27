87e216735d5bf93fc5205ddaa8f442048079ec4d
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-27 14:56:31 +0530

    update bot_data.py

--- userbot/plugins/bot_data.py
+++ userbot/plugins/bot_data.py
@@ -5,37 +5,21 @@ import asyncio
 
 from telethon import functions
 
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
 
 
-@borg.on(admin_cmd("(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    animation_interval = 1
-    animation_ttl = range(0, 27)
-    input_str = event.pattern_match.group(1)
-    if input_str == "mf":
-        await event.edit(input_str)
-        animation_chars = [
-            "\n......................................../´¯/)\n......................................,/¯../ \n...................................../..../ \n..................................../´.¯/\n..................................../´¯/\n..................................,/¯../ \n................................../..../ \n................................./´¯./\n................................/´¯./\n..............................,/¯../\n............................./..../ \n............................/´¯/\n........................../´¯./\n........................,/¯../ \n......................./..../ \n....................../´¯/\n....................,/¯../ \n.................../..../ \n............./´¯/'...'/´¯¯`·¸ \n........../'/.../..../......./¨¯\ \n........('(...´...´.... ¯~/'...') \n.........\.................'...../ \n..........''...\.......... _.·´ \n............\..............( \n..............\.............\..."
-        ]
-
-        for i in animation_ttl:
-
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 27])
-
-
-@borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
+@bot.on(admin_cmd(pattern="dc", outgoing=True))  # pylint:disable=E0602
+@bot.on(sudo_cmd(pattern="dc", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     result = await borg(functions.help.GetNearestDcRequest())  # pylint:disable=E0602
-    await event.edit(result.stringify())
+    await edit_or_reply(event, result.stringify())
 
 
-@borg.on(admin_cmd(pattern="config"))  # pylint:disable=E0602
+@bot.on(admin_cmd(pattern="config", outgoing=True))  # pylint:disable=E0602
+@bot.on(sudo_cmd(pattern="config", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
