f27be2ac66f43391f8dd2f267d8e584d81738924
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-28 15:37:37 +0530

    update pat.py

--- userbot/plugins/pat.py
+++ userbot/plugins/pat.py
@@ -1,11 +1,7 @@
-﻿"""
+"""
 HeadPat Module for Userbot (http://headp.at)
 cmd:- .pat username or reply to msg
 By:- git: jaskaranSM tg: @Zero_cool7870
-
-
-
-
 """
 
 
@@ -15,19 +11,21 @@ from urllib import parse
 
 import requests
 
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
 
 BASE_URL = "https://headp.at/pats/{}"
 PAT_IMAGE = "pat.jpg"
 
 
-@borg.on(admin_cmd(pattern="pat ?(.*)", outgoing=True))
+@bot.on(admin_cmd(pattern="pat ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="pat ?(.*)", allow_sudo=True))
 async def lastfm(event):
     if event.fwd_from:
         return
     username = event.pattern_match.group(1)
     if not username and not event.reply_to_msg_id:
-        await event.edit("`Reply to a message or provide username`")
+        await edit_or_reply(event, "`Reply to a message or provide username`")
         return
 
     resp = requests.get("http://headp.at/js/pats.json")
@@ -41,3 +39,8 @@ async def lastfm(event):
     else:
         await borg.send_file(event.chat_id, PAT_IMAGE, reply_to=event.reply_to_msg_id)
     remove(PAT_IMAGE)
+
+
+CmdHelp("pat").add_command(
+  "pat", "<reply>", "Gives the replied user a pat"
+).add()
