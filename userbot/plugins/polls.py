bb036def0c6cc77aac70d17962412cf58fe1c040
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-28 20:49:17 +0530

    update polls.py

--- userbot/plugins/polls.py
+++ userbot/plugins/polls.py
@@ -1,18 +1,18 @@
 """Get Poll Info on non supported clients
 Syntax: .get_poll"""
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
 
 
-@borg.on(admin_cmd(pattern="get_poll"))
+@bot.on(admin_cmd(pattern="get_poll$", outgoing=True))
+@bot.on(sudo_cmd(pattern="get_poll$", allow_sudo=True))
 async def _(event):
     reply_message = await event.get_reply_message()
     if reply_message.media is None:
-        await event.edit(
-            "Please reply to a media_type == @gPoll to view the questions and answers"
+        await edit_or_reply(event, "Please reply to a media_type == @gPoll to view the questions and answers"
         )
     elif reply_message.media.poll is None:
-        await event.edit(
-            "Please reply to a media_type == @gPoll to view the questions and answers"
+        await edit_or_reply(event, "Please reply to a media_type == @gPoll to view the questions and answers"
         )
     else:
         media = reply_message.media
@@ -37,4 +37,8 @@ Answers: \n""".format(
         else:
             for answer in answers:
                 edit_caption += "{}> {}\n".format(answer.option, answer.text)
-        await event.edit(edit_caption)
+        await edit_or_reply(event, edit_caption)
+
+CmdHelp("polls").add_command(
+  "get_poll", "<reply>", "Gets the details about the replied poll."
+).add()
