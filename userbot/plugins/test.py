4fb0bea67f9832455314ecc0d2b12fdccb985bc1
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-29 21:34:06 +0530

    update test.py

--- userbot/plugins/test.py
+++ userbot/plugins/test.py
@@ -3,23 +3,39 @@
 
 import asyncio
 
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
 
 
-@borg.on(admin_cmd(pattern="test ?(.*)"))
+@bot.on(admin_cmd(pattern="test ?(.*)"))
+@bot.on(sudo_cmd(pattern="test ?(.*)", allow_sudo=True))
 async def _(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
 
-        await event.edit("`Testing Hêllẞø†....`")
+        await edit_or_reply(event, "`Testing Hêllẞø†`")
+        await asyncio.sleep(1)
+        await edit_or_reply(event, "`Testing Hêllẞø†.`")
+        await asyncio.sleep(1)
+        await edit_or_reply(event, "`Testing Hêllẞø†..`")
+        await asyncio.sleep(1)
+        await edit_or_reply(event, "`Testing Hêllẞø†...`")
+        await asyncio.sleep(1)
+        await edit_or_reply(event, "`Testing Hêllẞø†....`")
+        await asyncio.sleep(1)
+        await edit_or_reply(event, "`Testing Hêllẞø†.....`")
         await asyncio.sleep(2)
-        await event.edit("`Testing Hêllẞø†..`")
+        await edit_or_reply(event, "__Testing Successful__")
         await asyncio.sleep(2)
-        await event.edit("__Testing Successful__")
+        await edit_or_reply(event, "`Generating Output`\nPlease wait")
         await asyncio.sleep(2)
-        await event.edit("`Making Output` \n\nPlease wait")
+        await edit_or_reply(event, "__Output Generated Successfully__")
         await asyncio.sleep(2)
-        await event.edit("__Output Successful__")
+        await edit_or_reply(event, "**SAVING OUTPUT TO HELLBOT LOCAL DATABASE**")
         await asyncio.sleep(3.5)
-        await event.edit(
-            "Your[Hêllẞø†](https:/t.me/hellbot_official) is working Fine...\n       Join @HellBot_Official For Any Help......"
+        await edit_or_reply(event, 
+            "Your[Hêllẞø†](https:/t.me/hellbot_official) is working Fine...\n       Join @HellBot_Official_Chat For Any Help......"
         )
+
+CmdHelp("test").add_command(
+  "test", None, "Test wether your bot is running or not."
+).add()
