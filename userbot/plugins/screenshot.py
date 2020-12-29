0abee4200fcb6fdeca44888f858bc2b9ec41be31
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-29 13:54:42 +0530

    update screenshot.py

--- userbot/plugins/screenshot.py
+++ userbot/plugins/screenshot.py
@@ -8,17 +8,18 @@ from datetime import datetime
 
 from selenium import webdriver
 
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
 
-
-@borg.on(admin_cmd(pattern="screenshot (.*)"))
+@bot.on(admin_cmd(pattern="screenshot (.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="screenshot (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     if Config.GOOGLE_CHROME_BIN is None:
-        await event.edit("need to install Google Chrome. Module Stopping.")
+        await edit_or_reply(event, "need to install Google Chrome. Module Stopping.")
         return
-    await event.edit("Processing ...weit")
+    await edit_or_reply(event, "Processing ...weit")
     start = datetime.now()
     try:
         chrome_options = webdriver.ChromeOptions()
@@ -29,25 +30,25 @@ async def _(event):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
-        await event.edit("Starting Google Chrome BIN")
+        await edit_or_reply(event, "Starting Google Chrome BIN")
         driver = webdriver.Chrome(chrome_options=chrome_options)
         input_str = event.pattern_match.group(1)
         driver.get(input_str)
-        await event.edit("Calculating Page Dimensions")
+        await edit_or_reply(event, "Calculating Page Dimensions")
         height = driver.execute_script(
             "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
         )
         width = driver.execute_script(
             "return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);"
         )
-        await event.edit("Painting web-page")
+        await edit_or_reply(event, "Painting web-page")
         driver.set_window_size(width + 100, height + 100)
         # Add some pixels on top of the calculated dimensions
         # for good measure to make the scroll bars disappear
         im_png = driver.get_screenshot_as_png()
         # saves screenshot of entire page
         driver.close()
-        await event.edit("Stopping Google Chrome BIN")
+        await edit_or_reply(event, "Stopping Google Chrome BIN")
         message_id = event.message.id
         if event.reply_to_msg_id:
             message_id = event.reply_to_msg_id
@@ -64,6 +65,10 @@ async def _(event):
             )
         end = datetime.now()
         ms = (end - start).seconds
-        await event.edit(f"Completed screencapture Process in {ms} seconds")
+        await edit_or_reply(event, f"Completed screencapture Process in {ms} seconds")
     except Exception:
-        await event.edit(traceback.format_exc())
+        await edit_or_reply(event, traceback.format_exc())
+
+CmdHelp("screenshot").add_command(
+  "screenshot", "<link>", "Gives out the web screenshot of given link via Google Crome Bin in .png format", ".screenshot https://github.com/hellboy-op/hellbot"
+).add()
