c6d43ee83b0f5a7e8196f8eeda2dd4bac2d7380e
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-29 21:34:47 +0530

    Update stt.py

--- userbot/plugins/stt.py
+++ userbot/plugins/stt.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import requests
 
-from userbot.cmdhelp import admin_cmd, sudo_cmd, edit_or_reply
+from utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 
