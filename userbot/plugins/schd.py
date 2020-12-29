2fab55aedbd563cf16356fbf0aa56ff2a6872389
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-29 10:23:39 +0530

    update schd.py

--- userbot/plugins/schd.py
+++ userbot/plugins/schd.py
@@ -2,20 +2,22 @@
 Syntax: .schd <time_in_seconds> ;=; <message to send>"""
 import asyncio
 
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
 
 
-@borg.on(admin_cmd("schd ?(.*)"))
+@bot.on(admin_cmd(pattern="schd ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="schd ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
     ttl = 0
-    message = "SYNTAX: `.schd <time_in_seconds> = <message to send>`"
+    message = "SYNTAX: `.schd <time_in_seconds> - <message to send>`"
     if input_str:
         await event.delete()
-        if "=" in input_str:
-            ttl, message = input_str.split("=")
+        if "-" in input_str:
+            ttl, message = input_str.split("-")
         elif event.reply_to_msg_id:
             await event.delete()
             ttl = int(input_str)
@@ -24,3 +26,7 @@ async def _(event):
         await event.respond(message)
     else:
         await event.edit(message)
+
+CmdHelp("schedule").add_command(
+  "schd", "<time in secs> - <msg to send>", "Sends the message in given time(seconds)", ".schd 60 - Hello"
+).add()
