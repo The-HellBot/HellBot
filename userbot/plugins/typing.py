16dee3fdda10200f25f58f66c9afa8efbdb6bc46
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-30 21:34:27 +0530

    update typing.py

--- userbot/plugins/typing.py
+++ userbot/plugins/typing.py
@@ -3,10 +3,12 @@
 # (c) Shrimadhav U K
 import asyncio
 
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
 
 
-@borg.on(admin_cmd(pattern="type (.*)"))
+@bot.on(admin_cmd(pattern="type (.*)"))
+@bot.on(sudo_cmd(pattern="type (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -16,24 +18,29 @@ async def _(event):
     for i in range(601):
         shiiinabot += "\u2060"
     try:
-        await event.edit(shiiinabot)
+        await edit_or_reply(event, shiiinabot)
     except Exception as e:
         logger.warn(str(e))
-    typing_symbol = "|"
+    typing_symbol = "_"
     DELAY_BETWEEN_EDITS = 0.3
     previous_text = ""
-    await event.edit(typing_symbol)
+    await edit_or_reply(event, typing_symbol)
     await asyncio.sleep(DELAY_BETWEEN_EDITS)
     for character in input_str:
         previous_text = previous_text + "" + character
         typing_text = previous_text + "" + typing_symbol
         try:
-            await event.edit(typing_text)
+            await edit_or_reply(event, typing_text)
         except Exception as e:
             logger.warn(str(e))
         await asyncio.sleep(DELAY_BETWEEN_EDITS)
         try:
-            await event.edit(previous_text)
+            await edit_or_reply(event, previous_text)
         except Exception as e:
             logger.warn(str(e))
         await asyncio.sleep(DELAY_BETWEEN_EDITS)
+
+
+CmdHelp("type").add_command(
+  "type", "<text>", "A typing animation for given text. Try it out yourself"
+).add()
