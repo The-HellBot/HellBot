15a43a39bf22c67a5106201f96bdb489ade78614
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-30 12:51:39 +0530

    update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -12,7 +12,8 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 
 from .. import LOGS, TEMP_DOWNLOAD_DIRECTORY
-from ..utils import admin_cmd, edit_or_reply
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot.cmdhelp import CmdHelp
 
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
@@ -21,115 +22,14 @@ from ..utils import admin_cmd, edit_or_reply
 #
 # The entire source code is OSSRPL except 'whois' which is MPL
 # License: MPL and OSSRPL
+
 """ Userbot module for getiing info about any user on Telegram(including you!). """
 
-import html
-import os
 
-from requests import get
-from telethon.tl.functions.photos import GetUserPhotosRequest
-from telethon.tl.functions.users import GetFullUserRequest
-from telethon.tl.types import MessageEntityMentionName
-from telethon.utils import get_input_location
-
-from .. import LOGS, TEMP_DOWNLOAD_DIRECTORY
-from ..utils import admin_cmd, edit_or_reply
-
-
-@borg.on(admin_cmd(pattern="userinfo(?: |$)(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    replied_user, error_i_a = await get_full_user(event)
-    if replied_user is None:
-        return await edit_or_reply(event, f"`{str(error_i_a)}`")
-    user_id = replied_user.user.id
-    # some people have weird HTML in their names
-    first_name = html.escape(replied_user.user.first_name)
-    # https://stackoverflow.com/a/5072031/4723940
-    # some Deleted Accounts do not have first_name
-    if first_name is not None:
-        # some weird people (like me) have more than 4096 characters in their
-        # names
-        first_name = first_name.replace("\u2060", "")
-    # inspired by https://telegram.dog/afsaI181
-    common_chats = replied_user.common_chats_count
-    try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
-    except:
-        dc_id = "Couldn't fetch DC ID!"
-    try:
-        casurl = "https://api.cas.chat/check?user_id={}".format(user_id)
-        data = get(casurl).json()
-    except Exception as e:
-        LOGS.info(e)
-        data = None
-    if data:
-        if data["ok"]:
-            cas = "**Antispam(CAS) Banned :** `True`"
-        else:
-            cas = "**Antispam(CAS) Banned :** `False`"
-    else:
-        cas = "**Antispam(CAS) Banned :** `Couldn't Fetch`"
-    caption = """**Info of [{}](tg://user?id={}):
-   -🔖ID : **`{}`
-   **-**👥**Groups in Common : **`{}`
-   **-**🌏**Data Centre Number : **`{}`
-   **-**🔏**Restricted by telegram : **`{}`
-   **-**🦅{}
-   **-**👮‍♂️{}
-""".format(
-        first_name,
-        user_id,
-        user_id,
-        common_chats,
-        dc_id,
-        replied_user.user.restricted,
-        sw,
-        cas,
-    )
-    await event.edit(caption)
-
-
-async def get_full_user(event):
-    input_str = event.pattern_match.group(1)
-    if input_str:
-        try:
-            try:
-                input_str = int(input_str)
-            except:
-                pass
-            user_object = await event.client.get_entity(input_str)
-            user_id = user_object.id
-            replied_user = await event.client(GetFullUserRequest(user_id))
-            return replied_user, None
-        except Exception as e:
-            return None, e
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        if previous_message.forward:
-            replied_user = await event.client(
-                GetFullUserRequest(
-                    previous_message.forward.from_id
-                    or previous_message.forward.channel_id
-                )
-            )
-            return replied_user, None
-        replied_user = await event.client(GetFullUserRequest(previous_message.from_id))
-        return replied_user, None
-    if event.is_private:
-        try:
-            user_id = event.chat_id
-            replied_user = await event.client(GetFullUserRequest(user_id))
-            return replied_user, None
-        except Exception as e:
-            return None, e
-    return None, "No input is found"
-
-
-@borg.on(admin_cmd(pattern="whois(?: |$)(.*)"))
+@bot.on(admin_cmd(pattern="whois(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="whois(?: |$)(.*)", allow_sudo=True))
 async def who(event):
-    cat = await edit_or_reply(
+    hell = await edit_or_reply(
         event, "`Sit tight while I steal some data from This guuyyy...`"
     )
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
@@ -155,9 +55,9 @@ async def who(event):
         )
         if not photo.startswith("http"):
             os.remove(photo)
-        await cat.delete()
+        await hell.delete()
     except TypeError:
-        await cat.edit(caption, parse_mode="html")
+        await hell.edit(caption, parse_mode="html")
 
 
 async def get_user(event):
@@ -226,22 +126,23 @@ async def fetch_info(replied_user, event):
     username = "@{}".format(username) if username else ("This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
     caption = "<b>USER INFO from Hêllẞø†'s database :</b>\n\n"
-    caption += f"👤First Name: {first_name} {last_name}\n"
-    caption += f"🤵Username: {username}\n"
-    caption += f"🔖ID: <code>{user_id}</code>\n"
-    caption += f"🌏Data Centre ID: {dc_id}\n"
-    caption += f"🖼Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"🤖Is Bot: {is_bot}\n"
-    caption += f"🔏Is Restricted: {restricted}\n"
-    caption += f"🌐Is Verified by Telegram: {verified}\n\n"
-    caption += f"✍️Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"👥Common Chats with this user: {common_chat}\n"
-    caption += f"🔗Permanent Link To Profile: "
+    caption += f"👤 First Name: {first_name} {last_name}\n"
+    caption += f"🤵 Username: {username}\n"
+    caption += f"🔖 ID: <code>{user_id}</code>\n"
+    caption += f"🌏 Data Centre ID: {dc_id}\n"
+    caption += f"🖼 Number of Profile Pics: {replied_user_profile_photos_count}\n"
+    caption += f"🤖 Is Bot: {is_bot}\n"
+    caption += f"🔏 Is Restricted: {restricted}\n"
+    caption += f"🌐 Is Verified by Telegram: {verified}\n\n"
+    caption += f"✍️ Bio: \n<code>{user_bio}</code>\n\n"
+    caption += f"👥 Common Chats with this user: {common_chat}\n"
+    caption += f"🔗 Permanent Link To Profile: "
     caption += f'<a href="tg://user?id={user_id}">{first_name}</a>'
     return photo, caption
 
 
-@borg.on(admin_cmd(pattern="link(?: |$)(.*)"))
+@bot.on(admin_cmd(pattern="link(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="link(?: |$)(.*)", allow_sudo=True))
 async def permalink(mention):
     """ For .link command, generates a link to the user's PM with a custom text. """
     user, custom = await get_user_from_event(mention)
@@ -296,3 +197,11 @@ async def ge(user, event):
         await event.edit(str(err))
         return None
     return user_obj
+
+CmdHelp("infos").add_command(
+  "whois", "<reply to user>", "Extracts user info"
+).add_command(
+  "info", "<reply to user>", "Same as .whois"
+).add_command(
+  "link", "<reply to user>", "Creates a permanent link to a user profile. With custom tag if mentioned"
+).add()
