6f858acbbbda73875467093e28d4ea363b91c0e3
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-01-01 00:05:50 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -9,7 +9,7 @@ from userbot.uniborgConfig import Config
 from var import Var
 
 StartTime = time.time()
-hellversion = "1.9" 
+hellversion = "2.0" 
 
 os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
