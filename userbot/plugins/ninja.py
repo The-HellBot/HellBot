a027755b8551cb1d9d06129de158bbeee963a081
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-01-04 07:56:27 +0530

    update ninja.py

--- userbot/plugins/ninja.py
+++ userbot/plugins/ninja.py
@@ -2,52 +2,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import asyncio
-
+from asyncio import sleep
 from telethon import events
 import telethon.utils
 
-from uniborg.util import admin_cmd,re
+from userbot.utils import admin_cmd, sudo_cmd, errors_handler
+from userbot import bot as hellbot
 
 
-async def get_target_message(event):
-    if event.is_reply and (await event.get_reply_message()).from_id == borg.uid:
-        return await event.get_reply_message()
-    async for message in borg.iter_messages(
-            await event.get_input_chat(), limit=20):
-        if message.out:
-            return message
-
-
-async def await_read(chat, message):
-    chat = client.get_peer_id(chat)
-
-    async def read_filter(read_event):
-        return (read_event.chat_id == chat
-                and read_event.is_read(message))
-    fut = borg.await_event(events.MessageRead(inbox=False), read_filter)
-
-    if await is_read(borg, chat, message):
-        fut.cancel()
-        return
-
-    await fut
-
-
-@borg.on(admin_cmd(pattern="del ?(.*)"))
-async def delete(event):
-    await event.delete()
-    command = event.pattern_match.group(1)
- #   if command == 'del':
-       # text = event.pattern_match.group(2)
-       # if not text:
-         #   return
-    target = await get_target_message(event)
-    if target:
-        chat = await event.get_input_chat()
-        await await_read(chat, target)
-        await asyncio.sleep(.5)
-     #   if command == 'del':
-        await borg.edit_message(chat, target, text)
-    else:
-          await borg.delete_messages(chat, target, revoke=True)
+@hellbot.on(admin_cmd(outgoing=True, pattern="del$"))
+@hellbot.on(sudo_cmd(allow_sudo=True, pattern="del$"))
+@errors_handler
+async def delete_it(safai):
+    """ For .del command, delete the replied message. """
+    msg_src = await safai.get_reply_message()
+    if safai.reply_to_msg_id:
+        try:
+            await msg_src.delete()
+            await safai.delete()
