ab137bda239027c2c58bb6c6724810c7a0a75065
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-01-08 12:46:39 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -4,23 +4,14 @@ import random
 from colorama import Fore, Style
 
 
-kraken = """                                                                                            
-HHHHHHHHH     HHHHHHHHH                   lllllll lllllll       BBBBBBBBBBBBBBBBB                             tttt          
-H:::::::H     H:::::::H                   l:::::l l:::::l       B::::::::::::::::B                         ttt:::t          
-H:::::::H     H:::::::H                   l:::::l l:::::l       B::::::BBBBBB:::::B                        t:::::t          
-HH::::::H     H::::::HH                   l:::::l l:::::l       BB:::::B     B:::::B                       t:::::t          
-  H:::::H     H:::::H      eeeeeeeeeeee    l::::l  l::::l         B::::B     B:::::B   ooooooooooo   ttttttt:::::ttttttt    
-  H:::::H     H:::::H    ee::::::::::::ee  l::::l  l::::l         B::::B     B:::::B oo:::::::::::oo t:::::::::::::::::t    
-  H::::::HHHHH::::::H   e::::::eeeee:::::eel::::l  l::::l         B::::BBBBBB:::::B o:::::::::::::::ot:::::::::::::::::t    
-  H:::::::::::::::::H  e::::::e     e:::::el::::l  l::::l         B:::::::::::::BB  o:::::ooooo:::::otttttt:::::::tttttt    
-  H:::::::::::::::::H  e:::::::eeeee::::::el::::l  l::::l         B::::BBBBBB:::::B o::::o     o::::o      t:::::t          
-  H::::::HHHHH::::::H  e:::::::::::::::::e l::::l  l::::l         B::::B     B:::::Bo::::o     o::::o      t:::::t          
-  H:::::H     H:::::H  e::::::eeeeeeeeeee  l::::l  l::::l         B::::B     B:::::Bo::::o     o::::o      t:::::t          
-  H:::::H     H:::::H  e:::::::e           l::::l  l::::l         B::::B     B:::::Bo::::o     o::::o      t:::::t    tttttt
-HH::::::H     H::::::HHe::::::::e         l::::::ll::::::l      BB:::::BBBBBB::::::Bo:::::ooooo:::::o      t::::::tttt:::::t
-H:::::::H     H:::::::H e::::::::eeeeeeee l::::::ll::::::l      B:::::::::::::::::B o:::::::::::::::o      tt::::::::::::::t
-H:::::::H     H:::::::H  ee:::::::::::::e l::::::ll::::::l      B::::::::::::::::B   oo:::::::::::oo         tt:::::::::::tt
-HHHHHHHHH     HHHHHHHHH    eeeeeeeeeeeeee llllllllllllllll      BBBBBBBBBBBBBBBBB      ooooooooooo             ttttttttttt  
+kraken = """
+╔╗──╔═══╦═══╦═══╦═╗─╔╦═══╦═══╦═══╦╗──╔╗    ╔═══╦═══╗     ╔═══╦╗─╔╦═══╦╗╔═╗
+║║──║╔══╣╔═╗║╔══╣║╚╗║╠╗╔╗║╔═╗║╔═╗║╚╗╔╝║    ║╔═╗║╔═╗║     ║╔══╣║─║║╔═╗║║║╔╝
+║║──║╚══╣║─╚╣╚══╣╔╗╚╝║║║║║║─║║╚═╝╠╗╚╝╔╝    ║║─║║╚══╗     ║╚══╣║─║║║─╚╣╚╝╝
+║║─╔╣╔══╣║╔═╣╔══╣║╚╗║║║║║║╚═╝║╔╗╔╝╚╗╔╝     ║╚═╝╠══╗║     ║╔══╣║─║║║─╔╣╔╗║
+║╚═╝║╚══╣╚╩═║╚══╣║─║║╠╝╚╝║╔═╗║║║╚╗─║║      ║╔═╗║╚═╝║     ║║──║╚═╝║╚═╝║║║╚╗
+╚═══╩═══╩═══╩═══╩╝─╚═╩═══╩╝─╚╩╝╚═╝─╚╝      ╚╝─╚╩═══╝     ╚╝──╚═══╩═══╩╝╚═╝
+#ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt
 """
                                                                                                             
 print("")
