bef50facb9b360b6fc53103b9c074d5f96af421c
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-01-08 13:30:28 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -15,7 +15,7 @@ kraken = """
 """
                                                                                                             
 print("")
-print(Style.BRIGHT + Fore.BLACK + Back.BLUE + kraken)
+print(Style.BRIGHT + Fore.YELLOW + Back.CYAN + kraken)
 print(Style.RESET_ALL)
 print("""Welcome To HellBot String Generator By @Kraken_The_BadASS""")
 print("""Kindly Enter Your Details To Continue ! """)
