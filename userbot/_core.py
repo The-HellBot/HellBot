6e21e2b50a9b91ed4181f4920a466507ecad46da
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-01-10 19:36:22 +0530

    update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -19,7 +19,7 @@ hell_logo = "./KRAKEN/hellbot_logo.jpg"
 async def send(event):
     if event.fwd_from:
         return
-    kraken = bot.uid
+    kraken = borg.uid
     message_id = event.message.id
     thumb = hell_logo
     input_str = event.pattern_match.group(1)
