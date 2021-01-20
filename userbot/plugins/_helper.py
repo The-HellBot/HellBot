b9c12243a3ddb426dccaf7e10f201126bd2886fa
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-01-20 14:58:49 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -8,7 +8,7 @@ from userbot import ALIVE_NAME, CMD_LIST, SUDO_LIST
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
-@bot.on(admin_cmd(pattern="help", outgoing=True))
+@bot.on(admin_cmd(pattern="help ?(.*)", outgoing=True))
 async def yardim(event):
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
     hell_input = event.pattern_match.group(1)
