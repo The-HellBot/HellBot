00bf700b5a055776b64f8bddfa4fd21d6bf336ff
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-01-23 15:54:28 +0530

    Update blacklist.py

--- userbot/plugins/blacklist.py
+++ userbot/plugins/blacklist.py
@@ -11,7 +11,7 @@ import re
 
 from telethon import events
 
-import userbot.plugins.sql_helper.blacklist_sql as sql
+from userbot.plugins.sql_helper import blacklist_sql as sql
 from userbot import CMD_HELP
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
