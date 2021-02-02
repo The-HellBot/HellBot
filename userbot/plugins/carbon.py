15f0be02fd3aaa2095395cb7ccb2ceefdc278f82
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-02-02 13:07:12 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -20,6 +20,8 @@ LANG = "en"
 @bot.on(admin_cmd(outgoing=True, pattern="carbon(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="carbon(?: |$)(.*)", allow_sudo=True))
 async def carbon_api(e):
+    if e.fwd_from:
+        return
     """ A Wrapper for carbon.now.sh """
     await e.edit("`Processing..`")
     CARBON = "https://carbon.now.sh/?l={lang}&code={code}"
@@ -82,6 +84,8 @@ async def carbon_api(e):
 @bot.on(admin_cmd(outgoing=True, pattern="krb"))
 @bot.on(sudo_cmd(pattern="krb", allow_sudo=True))
 async def carbon_api(e):
+    if e.fwd_from:
+        return
     hell = await edit_or_reply(e, "`Processing....`")
     CARBON = "https://carbon.now.sh/?l={lang}&code={code}"
     textx = await e.get_reply_message()
@@ -164,6 +168,8 @@ async def carbon_api(e):
 @bot.on(admin_cmd(pattern=f"kar1(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="kar1(?: |$)(.*)", allow_sudo=True))
 async def carbon_api(e):
+    if e.fwd_from:
+        return
     """ A Wrapper for carbon.now.sh """
     hell = await edit_or_reply(e, "🔲🔲🔲🔲🔲")
     CARBON = "https://carbon.now.sh/?bg=rgba(249%2C237%2C212%2C0)&t=synthwave-84&wt=none&l=application%2Fjson&ds=true&dsyoff=20px&dsblur=0px&wc=true&wa=true&pv=56px&ph=0px&ln=false&fl=1&fm=IBM%20Plex%20Mono&fs=14.5px&lh=153%25&si=false&es=4x&wm=false&code={code}"
@@ -223,6 +229,8 @@ async def carbon_api(e):
 @bot.on(admin_cmd(pattern=f"kar2(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="kar2(?: |$)(.*)", allow_sudo=True))
 async def carbon_api(e):
+    if e.fwd_from:
+        return
     """ A Wrapper for carbon.now.sh """
     hell = await edit_or_reply(e, "📛📛📛📛📛")
     CARBON = "https://carbon.now.sh/?bg=rgba(239%2C40%2C44%2C1)&t=one-light&wt=none&l=application%2Ftypescript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=143%25&si=false&es=2x&wm=false&code={code}"
@@ -282,6 +290,8 @@ async def carbon_api(e):
 @bot.on(admin_cmd(pattern=f"kar3(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="kar3(?: |$)(.*)", allow_sudo=True))
 async def carbon_api(e):
+    if e.fwd_from:
+        return
     """ A Wrapper for carbon.now.sh """
     hell = await edit_or_reply(e, "🎛🎛🎛🎛🎛")
     CARBON = "https://carbon.now.sh/?bg=rgba(74%2C144%2C226%2C1)&t=material&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Fira%20Code&fs=14px&lh=152%25&si=false&es=2x&wm=false&code={code}"
@@ -345,6 +355,8 @@ async def carbon_api(e):
 @bot.on(admin_cmd(pattern=f"kar4(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="kar4(?: |$)(.*)", allow_sudo=True))
 async def carbon_api(e):
+    if e.fwd_from:
+        return
     """ A Wrapper for carbon.now.sh """
     hell = await edit_or_reply(e, "🌚🌚🌚🌚🌚")
     CARBON = "https://carbon.now.sh/?bg=rgba(29%2C40%2C104%2C1)&t=one-light&wt=none&l=application%2Ftypescript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=143%25&si=false&es=2x&wm=false&code={code}"
@@ -408,6 +420,8 @@ async def carbon_api(e):
 @bot.on(admin_cmd(pattern=f"rgbk2(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="rgbk2(?: |$)(.*)", allow_sudo=True))
 async def carbon_api(e):
+    if e.fwd_from:
+        return
     RED = random.randint(0, 256)
     GREEN = random.randint(0, 256)
     BLUE = random.randint(0, 256)
@@ -475,6 +489,8 @@ async def carbon_api(e):
 @bot.on(admin_cmd(pattern=f"kargb(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="kargb(?: |$)(.*)", allow_sudo=True))
 async def carbon_api(e):
+    if e.fwd_from:
+        return
     RED = random.randint(0, 256)
     GREEN = random.randint(0, 256)
     BLUE = random.randint(0, 256)
