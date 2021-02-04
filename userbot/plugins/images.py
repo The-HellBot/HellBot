35521d4c2c6cc9dd0d8f7e0d4c3dbceb396cb30e
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-02-04 11:02:08 +0530

    Update images.py

--- userbot/plugins/images.py
+++ userbot/plugins/images.py
@@ -1,80 +1,52 @@
-
+# Credit To Sandy - CATUSERBOT
+# DON'T EDIT CREDITS..
+# 
+#
 import os
 import shutil
 from re import findall
 
 from userbot.google_imgs import googleimagesdownload
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
 
-
-@borg.on(admin_cmd(pattern="img ?(.*)"))
-
+@bot.on(admin_cmd(pattern=r"img(?: |$)(\d*)? ?(.*)"))
+@bot.on(sudo_cmd(pattern=r"img(?: |$)(\d*)? ?(.*)", allow_sudo=True))
 async def img_sampler(event):
-
-    await event.edit("`Processing...`")
-
-    reply = await event.get_reply_message()
-
-    if event.pattern_match.group(1):
-
-        query = event.pattern_match.group(1)
-
-    elif reply:
-
-        query = reply.message
-
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if event.is_reply and not event.pattern_match.group(2):
+        query = await event.get_reply_message()
+        query = str(query.message)
     else:
-
-    	await event.edit("`um, mind mentioning what I actually need to search for ;_;`")
-
-    	return
-
-        
-
-    lim = findall(r"lim=\d+", query)
-
-    # lim = event.pattern_match.group(1)
-
-    try:
-
-        lim = lim[0]
-
-        lim = lim.replace("lim=", "")
-
-        query = query.replace("lim=" + lim[0], "")
-
-    except IndexError:
-
-        lim = 5
-
+        query = str(event.pattern_match.group(2))
+    if not query:
+        return await edit_or_reply(
+            event, "Reply to a message or pass a query to search!"
+        )
+    cat = await edit_or_reply(event, "`Processing...`")
+    if event.pattern_match.group(1) != "":
+        lim = int(event.pattern_match.group(1))
+        if lim > 10:
+            lim = int(10)
+        if lim <= 0:
+            lim = int(1)
+    else:
+        lim = int(3)
     response = googleimagesdownload()
-
-
-
     # creating list of arguments
-
     arguments = {
-
         "keywords": query,
-
         "limit": lim,
-
         "format": "jpg",
-
-        "no_directory": "no_directory"
-
+        "no_directory": "no_directory",
     }
-
-
-
     # passing the arguments to the function
-
-    paths = response.download(arguments)
-
+    try:
+        paths = response.download(arguments)
+    except Exception as e:
+        return await cat.edit(f"Error: \n`{e}`")
     lst = paths[0][query]
-
-    await event.client.send_file(await event.client.get_input_entity(event.chat_id), lst)
-
+    await event.client.send_file(event.chat_id, lst, reply_to=reply_to_id)
     shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
-
-    await event.delete()
+    await cat.delete()
