16896157eee9fa9b0222b384065f0274a54fac9b
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-02-08 10:06:07 +0530

    Update ytdl.py

--- userbot/plugins/ytdl.py
+++ userbot/plugins/ytdl.py
@@ -89,6 +89,8 @@ def time_formatter(milliseconds: int) -> str:
 @bot.on(admin_cmd(pattern="yt(a|v) (.*)"))
 @bot.on(sudo_cmd(pattern="yt(a|v) (.*)", allow_sudo=True))
 async def download_video(v_url):
+    if v_url.fwd_from:
+        return
     """ For .ytdl command, download media from YouTube and many other sites. """
     url = v_url.pattern_match.group(2)
     type = v_url.pattern_match.group(1).lower()
