9b091db612ab99d0957ef33a63f3c38c3812c49e
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-02-14 16:10:18 +0530

    Update bot_alive.py

--- userbot/plugins/bot/bot_alive.py
+++ userbot/plugins/bot/bot_alive.py
@@ -1,7 +1,6 @@
-from userbot import *
-from userbot.utils import *
+from userbot import ALIVE_NAME, bot, hellversion
 from userbot.Config import Config
-from telethon import events, version
+from telethon import version
 from telethon.events import NewMessage
 from telethon.tl.custom import Dialog
 from telethon.tl.types import Channel, Chat, User
