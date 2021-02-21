24d683b754d2109f18579baaaffe5c686d0da951
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-02-22 04:09:34 +0530

    Create Config.py

---
+++ userbot/Config.py
@@ -0,0 +1,9 @@
+import os
+
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+    from sample_config import Config
+else:
+    if os.path.exists("config.py"):
+        from config import Development as Config
