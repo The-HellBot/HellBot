4db901b05136e85c354e5f66b7f0813c6bf22e1f
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-02-22 20:41:07 +0530

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -12,7 +12,8 @@ if ENV:
         SCREEN_SHOT_LAYER_ACCESS_KEY = os.environ.get("SCREEN_SHOT_LAYER_ACCESS_KEY", None)
         # Send .get_id in any group to fill this value.
         SUDO_COMMAND_HAND_LER = os.environ.get("SUDO_COMMAND_HAND_LER", r"\.")
-        
+        COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", r"\.")
+  
         # This is required for the plugins involving the file system.
         TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
         # This is required for the speech to text module. Get your USERNAME from https://console.bluemix.net/docs/services/speech-to-text/getting-started.html
