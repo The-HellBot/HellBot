f70094d96a7a4ae2c888501784d9b97becd6ec6a
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-02-22 14:33:06 +0530

    Update var.py

--- var.py
+++ var.py
@@ -3,7 +3,7 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    from sample_config import Config
+    from sample_config import Var
 else:
     if os.path.exists("config.py"):
         from config import Development as Config
