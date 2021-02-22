cda3f88b700443ac2d6145bfabc3e1599c583c8d
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-02-22 14:41:22 +0530

    Update var.py

--- var.py
+++ var.py
@@ -6,6 +6,4 @@ if ENV:
     from sample_config import Var
 else:
     if os.path.exists("config.py"):
-        from config import Development as Config
-
-Var = Config
+        from config import Development as Var
