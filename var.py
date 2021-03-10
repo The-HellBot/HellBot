4031fc7fe3fe6f1b7a16f7da3e592fcdfd36b207
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-10 16:10:37 +0530

    Delete var.py

--- var.py
+++
@@ -1,9 +0,0 @@
-import os
-
-ENV = bool(os.environ.get("ENV", False))
-
-if ENV:
-    from sample_config import Var
-else:
-    if os.path.exists("config.py"):
-        from config import Development as Var
