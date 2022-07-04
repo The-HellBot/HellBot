a293a7cf2127573511ad2da02b7cc150e6d6327d
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-07-04 12:06:52 +0530

    Update hell_string.py

--- hell_string.py
+++ hell_string.py
@@ -10,23 +10,23 @@ print("•••   HELLBOT  SESSION  GENERATOR   •••")
 print("\nHello!! Welcome to HellBot Session Generator\n")
 okvai = input("Enter 69 to continue: ")
 if okvai == "69":
-    print("Choose the string session type: \n1. HellBot \n2. Music Bot")
+    print("Choose the string session type: \n1. Telethon \n2. Pyrogram")
     library = input("\nYour Choice: ")
     if library == "1":
         print("\nTelethon Session For HellBot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
         with TelegramClient(StringSession(), APP_ID, API_HASH) as hellbot:
-            print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-            hellbot.send_message("me", f"#HELLBOT #HELLBOT_SESSION \n\n`{hellbot.session.save()}`")
+            print("\nYour HellBot Session is sent in your Telegram Saved Messages.")
+            hellbot.send_message("me", f"#HELLBOT #HELLBOT_SESSION #TELETHON\n\n`{hellbot.session.save()}`")
     elif library == "2":
-        print("Pyrogram Session for Music Bot")
+        print("Pyrogram Session for HellBot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
         with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as hellbot:
-            print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-            hellbot.send_message("me", f"#HELLBOT_MUSIC #HELLBOT_SESSION\n\n`{hellbot.export_session_string()}`")
+            print("\nYour HellBot Session is sent in your Telegram Saved Messages.")
+            hellbot.send_message("me", f"#HELLBOT #HELLBOT_SESSION #PYROGRAM\n\n`{hellbot.export_session_string()}`")
     else:
         print("Please Enter 1 or 2 only.")
 else:
-    print("Bhag jaa bhosdike")
+    print("Try Again!")
